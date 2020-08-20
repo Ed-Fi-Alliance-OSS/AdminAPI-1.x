84e5992e5cc14d31b2c5b6e400d6da01b263720c
j-jordan <9419827+j-jordan@users.noreply.github.com>
2020-08-20 15:38:41 -0500

    [AA-1010] Bump AppCommon to include version with new urlrewrite download link (#9)

--- EdFi.Suite3.Installer.AdminApp/prep-installer-package.ps1
+++ EdFi.Suite3.Installer.AdminApp/prep-installer-package.ps1
@@ -14,7 +14,7 @@ $ErrorActionPreference = "Stop"
 Push-Location $PackageDirectory
 
 $dependencyVersions = @{
-    AppCommon = "1.0.0-pre1089"
+    AppCommon = "1.0.0-pre1094"
 }
 
 # This is a hack for TeamCity - create empty ODS and Implementation directories so that

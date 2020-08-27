e6f62005dfe1ac2c83b252a67bef29eaf8322105
Jeremy Jordan <jeremy.jordan@headspring.com>
2020-08-27 12:56:48 -0500

    revert version bump

--- EdFi.Suite3.Installer.AdminApp/prep-installer-package.ps1
+++ EdFi.Suite3.Installer.AdminApp/prep-installer-package.ps1
@@ -14,7 +14,7 @@ $ErrorActionPreference = "Stop"
 Push-Location $PackageDirectory
 
 $dependencyVersions = @{
-    AppCommon = "1.0.0-pre1096"
+    AppCommon = "1.0.0-pre1095"
 }
 
 # This is a hack for TeamCity - create empty ODS and Implementation directories so that

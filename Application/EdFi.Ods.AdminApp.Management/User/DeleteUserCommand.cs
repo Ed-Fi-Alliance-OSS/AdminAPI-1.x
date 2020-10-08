e73ac56c6dc865112293670689f48a6872482065
Patrick Lioi <patrick@headspring.com>
2020-10-08 10:26:07 -0500

    Apply conditional compilation around `using` directives for types that have simply moved from one namespace to another for legacy ASP.NET Identity (net48) versus modern ASP.NET Identity Core (netcoreapp3.1).

--- Application/EdFi.Ods.AdminApp.Management/User/DeleteUserCommand.cs
+++ Application/EdFi.Ods.AdminApp.Management/User/DeleteUserCommand.cs
@@ -1,11 +1,15 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using System.Linq;
 using EdFi.Ods.AdminApp.Management.Database;
+#if NET48
 using Microsoft.AspNet.Identity.EntityFramework;
+#else
+using EdFi.Ods.AdminApp.Management.Database.Models;
+#endif
 
 namespace EdFi.Ods.AdminApp.Management.User
 {

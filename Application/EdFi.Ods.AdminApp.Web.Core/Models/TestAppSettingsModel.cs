81c4a6abf4152c7b2163b4a517310530438ee139
Saagar Minocha <saagar.minocha93@gmail.com>
2020-10-13 14:22:48 -0500

    Added a temporary controller, view and model to test the IOptions flow of getting the appSettings from the appSettings.json file. Added an appSettings.OnPremises.json file to override the base appSettings.

---
+++ Application/EdFi.Ods.AdminApp.Web.Core/Models/TestAppSettingsModel.cs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+namespace EdFi.Ods.AdminApp.Web.Models
+{
+    public class TestAppSettingsModel
+    {
+        public string Settings1 { get; set; }
+        public string Settings2 { get; set; }
+        public string Settings3 { get; set; }
+        public string Settings4 { get; set; }
+    }
+}

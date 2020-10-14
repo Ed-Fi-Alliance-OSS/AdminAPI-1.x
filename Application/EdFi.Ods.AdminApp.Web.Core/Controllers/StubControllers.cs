619c9e22420a0d14e04805f72a5a225ecd44911b
Patrick Lioi <patrick@headspring.com>
2020-10-13 19:53:22 -0500

    Include shared tab display enumerations and related models in .NET Core compilation.

---
+++ Application/EdFi.Ods.AdminApp.Web.Core/Controllers/StubControllers.cs
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+namespace EdFi.Ods.AdminApp.Web.Controllers
+{
+    public class GlobalSettingsController
+    {
+        public object Vendors() => throw new System.NotImplementedException();
+        public object AdvancedSettings() => throw new System.NotImplementedException();
+        public object LookAndFeel() => throw new System.NotImplementedException();
+        public object ClaimSets() => throw new System.NotImplementedException();
+        public object Users() => throw new System.NotImplementedException();
+    }
+
+    public class OdsInstanceSettingsController
+    {
+        public object Applications() => throw new System.NotImplementedException();
+        public object Descriptors() => throw new System.NotImplementedException();
+        public object Logging() => throw new System.NotImplementedException();
+        public object EducationOrganizations() => throw new System.NotImplementedException();
+        public object Setup() => throw new System.NotImplementedException();
+        public object BulkLoad() => throw new System.NotImplementedException();
+        public object LearningStandards() => throw new System.NotImplementedException();
+        public object SelectDistrict(int i) => throw new System.NotImplementedException();
+    }
+}

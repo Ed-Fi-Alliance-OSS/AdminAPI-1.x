13cb6260d4b856d047ba833a026e44e0be521fdc
Patrick Lioi <patrick@headspring.com>
2020-10-24 16:43:10 -0500

    Remove argument defaults, because all calls to ActionAjax specify all arguments.

--- Application/EdFi.Ods.AdminApp.Web/Helpers/HtmlHelperExtensions.cs
+++ Application/EdFi.Ods.AdminApp.Web/Helpers/HtmlHelperExtensions.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -431,7 +431,7 @@ namespace EdFi.Ods.AdminApp.Web.Helpers
             return input.WrapWith(inputContainer);
         }
 
-        public static HtmlTag ActionAjax(this HtmlHelper helper, string actionName, string controllerName, object routeValues, int minHeight = 0, string placeholderText = "")
+        public static HtmlTag ActionAjax(this HtmlHelper helper, string actionName, string controllerName, object routeValues, int minHeight, string placeholderText)
         {
             var url = new UrlHelper(helper.ViewContext.RequestContext).Action(actionName, controllerName, routeValues);
 

9ff1e884e3053676757dcac6f739185301387efd
Patrick Lioi <patrick@headspring.com>
2020-10-27 15:42:34 -0500

    Add warning comment to temporary code that needs to be revisited in AA-1120.

--- Application/EdFi.Ods.AdminApp.Web.Core/Helpers/TemporaryAuthenticationHelper.cs
+++ Application/EdFi.Ods.AdminApp.Web.Core/Helpers/TemporaryAuthenticationHelper.cs
@@ -7,6 +7,12 @@ namespace EdFi.Ods.AdminApp.Web.Helpers
         #if NET48
             return Request.IsAuthenticated();
         #else
+            //TODO: We must temporarily assume the user is authenticated,
+            //      until authentication under .NET Core becomes enabled
+            //      in AA-1120. At that time, this must begin returning
+            //      an accurate value similar to the NET48 implementation,
+            //      and at that time likely inline this method back to a simple
+            //      expression at the call site(s).
             return true;
         #endif
         }

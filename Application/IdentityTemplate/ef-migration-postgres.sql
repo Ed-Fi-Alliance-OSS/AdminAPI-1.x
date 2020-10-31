e29ce66f414561f192c81da427413da3147f9232
Patrick Lioi <patrick@headspring.com>
2020-10-31 13:30:21 -0500

    Include license headers in SQL scripts.

--- Application/IdentityTemplate/ef-migration-postgres.sql
+++ Application/IdentityTemplate/ef-migration-postgres.sql
@@ -1,4 +1,9 @@
-﻿CREATE TABLE AspNetRoles (
+﻿-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+CREATE TABLE AspNetRoles (
     Id text NOT NULL,
     Name character varying(256) NULL,
     NormalizedName character varying(256) NULL,

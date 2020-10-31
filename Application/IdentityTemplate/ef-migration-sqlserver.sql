72e2bbfd0406401930210407dcc6ad67f1f88b31
Patrick Lioi <patrick@headspring.com>
2020-10-31 13:57:18 -0500

    To match established naming conventions, uppercase SQL type names.

--- Application/IdentityTemplate/ef-migration-sqlserver.sql
+++ Application/IdentityTemplate/ef-migration-sqlserver.sql
@@ -4,72 +4,72 @@
 -- See the LICENSE and NOTICES files in the project root for more information.
 
 CREATE TABLE adminapp.Users (
-    Id nvarchar(450) NOT NULL,
-    UserName nvarchar(256) NULL,
-    NormalizedUserName nvarchar(256) NULL,
-    Email nvarchar(256) NULL,
-    NormalizedEmail nvarchar(256) NULL,
-    EmailConfirmed bit NOT NULL,
-    PasswordHash nvarchar(max) NULL,
-    SecurityStamp nvarchar(max) NULL,
-    ConcurrencyStamp nvarchar(max) NULL,
-    PhoneNumber nvarchar(max) NULL,
-    PhoneNumberConfirmed bit NOT NULL,
-    TwoFactorEnabled bit NOT NULL,
-    LockoutEnd datetimeoffset NULL,
-    LockoutEnabled bit NOT NULL,
-    AccessFailedCount int NOT NULL,
+    Id NVARCHAR(450) NOT NULL,
+    UserName NVARCHAR(256) NULL,
+    NormalizedUserName NVARCHAR(256) NULL,
+    Email NVARCHAR(256) NULL,
+    NormalizedEmail NVARCHAR(256) NULL,
+    EmailConfirmed BIT NOT NULL,
+    PasswordHash NVARCHAR(MAX) NULL,
+    SecurityStamp NVARCHAR(MAX) NULL,
+    ConcurrencyStamp NVARCHAR(MAX) NULL,
+    PhoneNumber NVARCHAR(MAX) NULL,
+    PhoneNumberConfirmed BIT NOT NULL,
+    TwoFactorEnabled BIT NOT NULL,
+    LockoutEnd DATETIMEOFFSET NULL,
+    LockoutEnabled BIT NOT NULL,
+    AccessFailedCount INT NOT NULL,
     CONSTRAINT PK_Users PRIMARY KEY (Id)
 );
 
 CREATE TABLE adminapp.Roles (
-    Id nvarchar(450) NOT NULL,
-    Name nvarchar(256) NULL,
-    NormalizedName nvarchar(256) NULL,
-    ConcurrencyStamp nvarchar(max) NULL,
+    Id NVARCHAR(450) NOT NULL,
+    Name NVARCHAR(256) NULL,
+    NormalizedName NVARCHAR(256) NULL,
+    ConcurrencyStamp NVARCHAR(MAX) NULL,
     CONSTRAINT PK_Roles PRIMARY KEY (Id)
 );
 
 CREATE TABLE adminapp.UserClaims (
-    Id int NOT NULL IDENTITY,
-    UserId nvarchar(450) NOT NULL,
-    ClaimType nvarchar(max) NULL,
-    ClaimValue nvarchar(max) NULL,
+    Id INT NOT NULL IDENTITY,
+    UserId NVARCHAR(450) NOT NULL,
+    ClaimType NVARCHAR(MAX) NULL,
+    ClaimValue NVARCHAR(MAX) NULL,
     CONSTRAINT PK_UserClaims PRIMARY KEY (Id),
     CONSTRAINT FK_UserClaims_Users_UserId FOREIGN KEY (UserId) REFERENCES adminapp.Users (Id) ON DELETE CASCADE
 );
 
 CREATE TABLE adminapp.RoleClaims (
-    Id int NOT NULL IDENTITY,
-    RoleId nvarchar(450) NOT NULL,
-    ClaimType nvarchar(max) NULL,
-    ClaimValue nvarchar(max) NULL,
+    Id INT NOT NULL IDENTITY,
+    RoleId NVARCHAR(450) NOT NULL,
+    ClaimType NVARCHAR(MAX) NULL,
+    ClaimValue NVARCHAR(MAX) NULL,
     CONSTRAINT PK_RoleClaims PRIMARY KEY (Id),
     CONSTRAINT FK_RoleClaims_Roles_RoleId FOREIGN KEY (RoleId) REFERENCES adminapp.Roles (Id) ON DELETE CASCADE
 );
 
 CREATE TABLE adminapp.UserLogins (
-    LoginProvider nvarchar(128) NOT NULL,
-    ProviderKey nvarchar(128) NOT NULL,
-    ProviderDisplayName nvarchar(max) NULL,
-    UserId nvarchar(450) NOT NULL,
+    LoginProvider NVARCHAR(128) NOT NULL,
+    ProviderKey NVARCHAR(128) NOT NULL,
+    ProviderDisplayName NVARCHAR(MAX) NULL,
+    UserId NVARCHAR(450) NOT NULL,
     CONSTRAINT PK_UserLogins PRIMARY KEY (LoginProvider, ProviderKey),
     CONSTRAINT FK_UserLogins_Users_UserId FOREIGN KEY (UserId) REFERENCES adminapp.Users (Id) ON DELETE CASCADE
 );
 
 CREATE TABLE adminapp.UserRoles (
-    UserId nvarchar(450) NOT NULL,
-    RoleId nvarchar(450) NOT NULL,
+    UserId NVARCHAR(450) NOT NULL,
+    RoleId NVARCHAR(450) NOT NULL,
     CONSTRAINT PK_UserRoles PRIMARY KEY (UserId, RoleId),
     CONSTRAINT FK_UserRoles_Roles_RoleId FOREIGN KEY (RoleId) REFERENCES adminapp.Roles (Id) ON DELETE CASCADE,
     CONSTRAINT FK_UserRoles_Users_UserId FOREIGN KEY (UserId) REFERENCES adminapp.Users (Id) ON DELETE CASCADE
 );
 
 CREATE TABLE adminapp.UserTokens (
-    UserId nvarchar(450) NOT NULL,
-    LoginProvider nvarchar(128) NOT NULL,
-    Name nvarchar(128) NOT NULL,
-    Value nvarchar(max) NULL,
+    UserId NVARCHAR(450) NOT NULL,
+    LoginProvider NVARCHAR(128) NOT NULL,
+    Name NVARCHAR(128) NOT NULL,
+    Value NVARCHAR(MAX) NULL,
     CONSTRAINT PK_UserTokens PRIMARY KEY (UserId, LoginProvider, Name),
     CONSTRAINT FK_UserTokens_Users_UserId FOREIGN KEY (UserId) REFERENCES adminapp.Users (Id) ON DELETE CASCADE
 );

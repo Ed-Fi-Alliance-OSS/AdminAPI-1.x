dc76c9cef64335418c1494a76bbeb92577f29058
Patrick Lioi <patrick@headspring.com>
2020-10-31 17:26:36 -0500

    Port IdentityController to .NET Core.

--- Application/EdFi.Ods.AdminApp.Web.Core/Controllers/IdentityController.cs
+++ Application/EdFi.Ods.AdminApp.Web.Core/Controllers/IdentityController.cs
@@ -6,14 +6,9 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
-#if NET48
-using System.Web.Mvc;
-#else
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-#endif
 using EdFi.Ods.AdminApp.Management.Configuration.Application;
 using EdFi.Ods.AdminApp.Management.Database.Models;
 using EdFi.Ods.AdminApp.Management.Identity;
@@ -23,9 +18,7 @@ using EdFi.Ods.AdminApp.Web.ActionFilters;
 using EdFi.Ods.AdminApp.Web.Infrastructure;
 using EdFi.Ods.AdminApp.Web.Models.ViewModels.Identity;
 using EdFi.Ods.AdminApp.Web.Models.ViewModels.User;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
+using Microsoft.AspNetCore.Authentication;
 
 namespace EdFi.Ods.AdminApp.Web.Controllers
 {
@@ -33,24 +26,32 @@ namespace EdFi.Ods.AdminApp.Web.Controllers
     public class IdentityController : ControllerBase
     {
         private readonly ApplicationConfigurationService _applicationConfiguration;
-        private ApplicationSignInManager _signInManager => HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
-        private ApplicationUserManager _userManager => HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-        private IAuthenticationManager _authenticationManager => HttpContext.GetOwinContext().Authentication;
+        private readonly SignInManager<AdminAppUser> _signInManager;
+        private readonly UserManager<AdminAppUser> _userManager;
         private readonly RegisterCommand _registerCommand;
         private readonly EditUserRoleCommand _editUserRoleCommand;
         private readonly IGetOdsInstanceRegistrationsQuery _getOdsInstanceRegistrationsQuery;
 
-        public IdentityController(ApplicationConfigurationService applicationConfiguration, RegisterCommand registerCommand, EditUserRoleCommand editUserRoleCommand, IGetOdsInstanceRegistrationsQuery getOdsInstanceRegistrationsQuery)
+        public IdentityController(ApplicationConfigurationService applicationConfiguration, RegisterCommand registerCommand, EditUserRoleCommand editUserRoleCommand, IGetOdsInstanceRegistrationsQuery getOdsInstanceRegistrationsQuery,
+            SignInManager<AdminAppUser> signInManager,
+            UserManager<AdminAppUser> userManager)
         {
             _applicationConfiguration = applicationConfiguration;
             _registerCommand = registerCommand;
             _editUserRoleCommand = editUserRoleCommand;
             _getOdsInstanceRegistrationsQuery = getOdsInstanceRegistrationsQuery;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [AllowAnonymous]
-        public ActionResult Login(string returnUrl)
+        public async Task<ActionResult> Login(string returnUrl)
         {
+            returnUrl ??= Url.Content("~/");
+
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
             ViewBag.ReturnUrl = returnUrl;
 
             return View(new LoginViewModel
@@ -63,24 +64,29 @@ namespace EdFi.Ods.AdminApp.Web.Controllers
         [AllowAnonymous]
         public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+            returnUrl ??= Url.Content("~/");
 
-            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: true);
-            switch (result)
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: true);
+
+            if (result.Succeeded)
             {
-                case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
-                case SignInStatus.LockedOut:
-                    return View("Lockout");
-                case SignInStatus.RequiresVerification:
-                    throw new NotSupportedException("Two-factor authentication is not supported.");
-                case SignInStatus.Failure:
-                default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
-                    return View(model);
+                return RedirectToLocal(returnUrl);
+            }
+            if (result.RequiresTwoFactor)
+            {
+                throw new NotSupportedException("Two-factor authentication is not supported.");
+            }
+            if (result.IsLockedOut)
+            {
+                return View("Lockout");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
             }
         }
 
@@ -111,7 +117,7 @@ namespace EdFi.Ods.AdminApp.Web.Controllers
                         UserId = adminAppUser.Id,
                         RoleId = Role.SuperAdmin.Value.ToString()
                     });
-                    await _signInManager.SignInAsync(adminAppUser, isPersistent: false, rememberBrowser: false);
+                    await _signInManager.SignInAsync(adminAppUser, isPersistent: false);
 
                     if (ZeroOdsInstanceRegistrations())
                         return RedirectToAction("RegisterOdsInstance", "OdsInstances");
@@ -136,19 +142,20 @@ namespace EdFi.Ods.AdminApp.Web.Controllers
         public async Task<ActionResult> ChangePassword(ChangePasswordViewModel model)
         {
             if (!ModelState.IsValid)
-            {
                 return View(model);
-            }
-            var result = await _userManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+
+            var result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+
             if (result.Succeeded)
             {
-                var user = await _userManager.FindByIdAsync(User.Identity.GetUserId());
-                if (user != null)
-                {
-                    user.RequirePasswordChange = false;
-                    await _userManager.UpdateAsync(user);
-                    await _signInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-                }
+                user.RequirePasswordChange = false;
+                await _userManager.UpdateAsync(user);
+                await _signInManager.RefreshSignInAsync(user);
 
                 TempData["PasswordChanged"] = true;
 
@@ -157,21 +164,22 @@ namespace EdFi.Ods.AdminApp.Web.Controllers
 
                 return RedirectToAction("ChangePassword");
             }
+
             AddErrors(result);
             return View(model);
         }
 
         [HttpPost]
-        public ActionResult LogOut()
+        public async Task<ActionResult> LogOut()
         {
-            _authenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
 
         private void AddErrors(IdentityResult result)
         {
             foreach (var error in result.Errors)
-                ModelState.AddModelError("", error);
+                ModelState.AddModelError("", error.Description);
         }
 
         private ActionResult RedirectToLocal(string returnUrl)

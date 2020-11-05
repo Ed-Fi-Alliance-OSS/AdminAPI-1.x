e6ab338b9021de1b4896d097c6a0ccfbe4b7f03b
Patrick Lioi <patrick@headspring.com>
2020-11-05 15:50:28 -0600

    As a step towards dramatically simplifying the DataTestBase hierarchy, begin by duplicating DataTestBase<T> as AdminAppDbContextTestBase<T> and using it as the base class for AdminAPpDatatestBase.

--- Application/EdFi.Ods.AdminApp.Management.Tests/AdminAppDataTestBase.cs
+++ Application/EdFi.Ods.AdminApp.Management.Tests/AdminAppDataTestBase.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -14,7 +14,7 @@ using NUnit.Framework;
 namespace EdFi.Ods.AdminApp.Management.Tests
 {
     [TestFixture]
-    public abstract class AdminAppDataTestBase : DataTestBase<AdminAppDbContext>
+    public abstract class AdminAppDataTestBase : AdminAppDbContextTestBase<AdminAppDbContext>
     {
         protected override string ConnectionString
         {

4e87d49a5c2ba07d5ad3b7952b8a08d2387f2e8e
Patrick Lioi <patrick@headspring.com>
2020-11-08 10:08:35 -0600

    Fix AppSettings lookups on .NET Core runs in CloudOdsAdminAppSettingsForAzure, EducationOrganizationTypes, and BulkImportService.

--- Application/EdFi.Ods.AdminApp.Web/Infrastructure/EducationOrganizationTypes.cs
+++ Application/EdFi.Ods.AdminApp.Web/Infrastructure/EducationOrganizationTypes.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -17,7 +17,7 @@ namespace EdFi.Ods.AdminApp.Web.Infrastructure
         }
 
         public static EducationOrganizationTypes Instance = _instance.Value;
-        public string LocalEducationAgency => ConfigurationHelper.GetAppSettings().LocalEducationAgencyTypeValue;
-        public string SchoolType => ConfigurationHelper.GetAppSettings().SchoolTypeValue;
+        public string LocalEducationAgency => CloudOdsAdminAppSettings.AppSettings.LocalEducationAgencyTypeValue;
+        public string SchoolType => CloudOdsAdminAppSettings.AppSettings.SchoolTypeValue;
     }
 }

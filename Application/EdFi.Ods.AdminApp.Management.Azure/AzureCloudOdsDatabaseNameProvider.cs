021194b6fddc2e9fbbb7ec9c5799f6efb2f50f9a
Patrick Lioi <patrick@headspring.com>
2020-11-21 16:33:54 -0600

    Collapse BaseCloudOdsDatabaseNameProvider down into its ownly subclass: AzureCloudOdsDatabaseNameProvider.

--- Application/EdFi.Ods.AdminApp.Management.Azure/AzureCloudOdsDatabaseNameProvider.cs
+++ Application/EdFi.Ods.AdminApp.Management.Azure/AzureCloudOdsDatabaseNameProvider.cs
@@ -1,11 +1,11 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
 namespace EdFi.Ods.AdminApp.Management.Azure
 {
-    public class AzureCloudOdsDatabaseNameProvider: BaseCloudOdsDatabaseNameProvider, ICloudOdsDatabaseNameProvider
+    public class AzureCloudOdsDatabaseNameProvider: ICloudOdsDatabaseNameProvider
     {
         public string GetDatabaseName(CloudOdsDatabases cloudOdsDatabase)
         {
@@ -28,6 +28,36 @@ namespace EdFi.Ods.AdminApp.Management.Azure
                 return GetPopulatedTemplateDatabaseName();
 
             return string.Empty;
+        }
+
+        private static string GetAdminDatabaseName()
+        {
+            return "EdFi_Admin";
+        }
+
+        private static string GetSecurityDatabaseName()
+        {
+            return "EdFi_Security";
+        }
+
+        private static string GetProductionOdsDatabaseName()
+        {
+            return "EdFi_Ods_Production";
+        }
+
+        private static string GetMasterDatabaseName()
+        {
+            return "master";
+        }
+
+        private static string GetMinimalTemplateDatabaseName()
+        {
+            return "EdFi_Ods_Minimal_Template";
+        }
+
+        private static string GetPopulatedTemplateDatabaseName()
+        {
+            return "EdFi_Ods_Populated_Template";
         }
     }
 }

c74aecb0e886f4d7a2fc7b221c747519afa0c26d
Patrick Lioi <patrick@headspring.com>
2020-11-22 13:18:37 -0600

    Move shared Infrastructure/ files from Web to Web.Core.

---
+++ Application/EdFi.Ods.AdminApp.Web.Core/Infrastructure/EducationOrganizationTypes.cs
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System;
+using EdFi.Ods.AdminApp.Management.Helpers;
+
+namespace EdFi.Ods.AdminApp.Web.Infrastructure
+{
+    public class EducationOrganizationTypes
+    {
+        private static readonly Lazy<EducationOrganizationTypes>  _instance = new Lazy<EducationOrganizationTypes>(() => new EducationOrganizationTypes());
+
+        protected EducationOrganizationTypes()
+        {
+        }
+
+        public static EducationOrganizationTypes Instance = _instance.Value;
+        public string LocalEducationAgency => CloudOdsAdminAppSettings.AppSettings.LocalEducationAgencyTypeValue;
+        public string SchoolType => CloudOdsAdminAppSettings.AppSettings.SchoolTypeValue;
+    }
+}
---
+++ Application/EdFi.Ods.AdminApp.Web.Core/Infrastructure/Tags/DefaultDisplayLabelBuilder.cs
@@ -0,0 +1,40 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using HtmlTags;
+using HtmlTags.Conventions;
+using HtmlTags.Conventions.Elements;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EdFi.Ods.AdminApp.Web.Infrastructure.Tags
+{
+    public class DefaultDisplayLabelBuilder : IElementBuilder
+    {
+        public bool Matches(ElementRequest subject)
+        {
+            return true;
+        }
+
+        public HtmlTag Build(ElementRequest request)
+        {
+            return new HtmlTag("strong").Text(BreakUpCamelCase(request.Accessor.Name) + ": ");
+        }
+
+        public static string BreakUpCamelCase(string fieldName)
+        {
+            var patterns = new[]
+            {
+                "([a-z])([A-Z])",
+                "([0-9])([a-zA-Z])",
+                "([a-zA-Z])([0-9])"
+            };
+
+            var output = patterns.Aggregate(fieldName,
+                (current, pattern) => Regex.Replace(current, pattern, "$1 $2", RegexOptions.IgnorePatternWhitespace));
+            return output.Replace('_', ' ');
+        }
+    }
+}

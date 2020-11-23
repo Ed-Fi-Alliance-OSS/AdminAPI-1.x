1c996b55bc40be1ae88fd185bbc42b4d6fd7a5ab
Patrick Lioi <patrick@headspring.com>
2020-11-22 18:51:28 -0600

    Remove trivial #if conditions now that we exclusivelyt arget netcoreapp3.1.

--- Application/EdFi.Ods.AdminApp.Management/Database/EntityFrameworkCoreDatabaseModelBuilderExtensions.cs
+++ Application/EdFi.Ods.AdminApp.Management/Database/EntityFrameworkCoreDatabaseModelBuilderExtensions.cs
@@ -3,7 +3,6 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-#if !NET48
 using System;
 using Microsoft.EntityFrameworkCore;
 
@@ -37,4 +36,3 @@ namespace EdFi.Ods.AdminApp.Management.Database
         }
     }
 }
-#endif

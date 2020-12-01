eedc8d5b1ea207460c3d26e8b9aa4b50c8be39a8
Saagar Minocha <saagar.minocha93@gmail.com>
2020-12-01 14:11:18 -0600

    Removed BulUpload specific injections from the OdsInstanceSettingsControllerFixture and added a new BulkUploadControllerFixture. Used BulkUpload models in the BulkUploadValidationTests.

--- Application/EdFi.Ods.AdminApp.Management.Tests/Controllers/OdsInstanceSettingsController/OdsInstanceSettingsControllerFixture.cs
+++ Application/EdFi.Ods.AdminApp.Management.Tests/Controllers/OdsInstanceSettingsController/OdsInstanceSettingsControllerFixture.cs
@@ -21,6 +21,7 @@ using System.Threading.Tasks;
 using EdFi.Ods.AdminApp.Management.Helpers;
 using Microsoft.Extensions.Options;
 using System.IO;
+using EdFi.Ods.AdminApp.Web.Models.ViewModels.BulkUpload;
 using Microsoft.AspNetCore.Hosting;
 using static EdFi.Ods.AdminApp.Management.Tests.Testing;
 
@@ -42,25 +43,13 @@ namespace EdFi.Ods.AdminApp.Management.Tests.Controllers.OdsInstanceSettingsCont
             CloudOdsSettingsService = new Mock<ICloudOdsSettingsService>();
             TabDisplayService = new Mock<ITabDisplayService>();
 
-            OdsSecretConfigurationProvider = new Mock<IOdsSecretConfigurationProvider>();
-            ApiConnectionInformationProvider = new Mock<IOdsApiConnectionInformationProvider>();
-            FileUploadHandler = new Mock<IFileUploadHandler>();
-            BulkUploadJob = new Mock<IBulkUploadJob>();
-            InstanceContext = new InstanceContext {Id = OdsInstanceId, Name = "TestOdsInstance", Description = "TestOdsInstance Description" };
-            InferOdsApiVersion = new Mock<IInferOdsApiVersion>();
-            var validationResult = new ValidationResult(new List<ValidationFailure>());
-            var bulkFileUploadValidator = new Mock<IValidator<BulkFileUploadModel>>();            
-            bulkFileUploadValidator.Setup(x
-                    => x.ValidateAsync(It.IsAny<BulkFileUploadModel>(), CancellationToken.None)).
-                Returns(Task.FromResult(validationResult));
+            InstanceContext = new InstanceContext { Id = OdsInstanceId, Name = "TestOdsInstance", Description = "TestOdsInstance Description" };
             AppSettings = new Mock<IOptions<AppSettings>>();
             Scoped<IOptions<AppSettings>>(appSettings =>
             {
                 AppSettings.Setup(x => x.Value).Returns(appSettings.Value);
             });
-            WebHostingEnvironment = new Mock<IWebHostEnvironment>();
-            WebHostingEnvironment.Setup(x => x.ContentRootPath).Returns(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-
+           
             SystemUnderTest = new Web.Controllers.OdsInstanceSettingsController(
                 OdsApiFacadeFactory.Object,
                 GetProductionApiProvisioningWarningsQuery.Object,
@@ -68,15 +57,8 @@ namespace EdFi.Ods.AdminApp.Management.Tests.Controllers.OdsInstanceSettingsCont
                 ProductionSetupJob.Object,
                 CloudOdsSettingsService.Object,
                 TabDisplayService.Object,
-                OdsSecretConfigurationProvider.Object,
-                ApiConnectionInformationProvider.Object,
-                FileUploadHandler.Object,
-                BulkUploadJob.Object,
                 InstanceContext,
-                InferOdsApiVersion.Object,
-                bulkFileUploadValidator.Object,
-                AppSettings.Object,
-                WebHostingEnvironment.Object
+                AppSettings.Object
             );
 
             AdditionalSetup();
@@ -88,15 +70,10 @@ namespace EdFi.Ods.AdminApp.Management.Tests.Controllers.OdsInstanceSettingsCont
         protected Mock<IProductionSetupJob> ProductionSetupJob;
         protected Mock<ICloudOdsSettingsService> CloudOdsSettingsService;
         protected Mock<ITabDisplayService> TabDisplayService;
-        protected Mock<IOdsSecretConfigurationProvider> OdsSecretConfigurationProvider;
-        protected Mock<IOdsApiConnectionInformationProvider> ApiConnectionInformationProvider;
-        protected Mock<IFileUploadHandler> FileUploadHandler;
-        protected Mock<IBulkUploadJob> BulkUploadJob;
+
         protected InstanceContext InstanceContext;
         protected Web.Controllers.OdsInstanceSettingsController SystemUnderTest;
         protected int OdsInstanceId = 1234;
-        protected Mock<IInferOdsApiVersion> InferOdsApiVersion;
         protected Mock<IOptions<AppSettings>> AppSettings;
-        protected Mock<IWebHostEnvironment> WebHostingEnvironment;
     }
 }

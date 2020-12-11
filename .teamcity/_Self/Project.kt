8f9c39410480c952f8cca80d8bd1b5ff6d86b57b
CSR2017 <34072978+CSR2017@users.noreply.github.com>
2020-12-11 15:15:56 -0600

    [AA-1189] Publish to azure artifacts (#102)
    * Add new build configuration step for publishing the admin app package to azure.
    
    * Rename build type object
    
    * Add new build type to project
    
    * Add env.VSS_NUGET_EXTERNAL_FEED_ENDPOINTS parameter
    
    * Update name to Publish to azure artifacts

--- .teamcity/_Self/Project.kt
+++ .teamcity/_Self/Project.kt
@@ -27,4 +27,5 @@ object AdminAppProject : Project({
     buildType(_self.buildTypes.BuildPullRequests)
     buildType(_self.buildTypes.Deploy)
     buildType(_self.buildTypes.PublishPackage)
+    buildType(_self.buildTypes.PublishPackageToAzureArtifacts)
 })

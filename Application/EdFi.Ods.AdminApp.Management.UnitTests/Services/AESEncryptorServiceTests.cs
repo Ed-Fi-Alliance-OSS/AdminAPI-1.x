c15db912f1ec1b59d80908b60b135da0c34eee1b
Stephen A. Fuqua <stephen@safnet.com>
2020-12-17 12:16:23 -0600

    Cleanup

--- Application/EdFi.Ods.AdminApp.Management.UnitTests/Services/AESEncryptorServiceTests.cs
+++ Application/EdFi.Ods.AdminApp.Management.UnitTests/Services/AESEncryptorServiceTests.cs
@@ -1,8 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
 using System;
-using System.IO;
-using System.Runtime.InteropServices.ComTypes;
-using System.Security.Cryptography;
-using System.Text;
 using EdFi.Ods.AdminApp.Management.Services;
 using NUnit.Framework;
 using Shouldly;
@@ -10,13 +11,13 @@ using Shouldly;
 namespace EdFi.Ods.AdminApp.Management.UnitTests.Services
 {
     [TestFixture]
+
     // ReSharper disable once InconsistentNaming - AES is proper, not Aes
     public class AESEncryptorServiceTests
     {
         // TODO: will need to have one test where the IV is calculated at random,
         // and we won't know what the actual result is.
-
-        // TODO: make sure the key is the right length
+        // TODO: make sure the key & iv is the right length and is base 64
 
         public const string TestKey = "bEnFYNociET2R1Wua3DHzwfU5u/Fa47N5fw0PXD0OSI=";
         public const string TestIv = "gpkAmCb03SJVbfBF0k798g==";
@@ -27,108 +28,6 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Services
         public const string EncryptMe = "encrypt me";
         public const string EncryptMeEncrypted =
             "gpkAmCb03SJVbfBF0k798g==.y2QnYoan80w8G8iDWRVxsEJqyqRGqwUwzCiiOH/NQvo=.KWELi015RE6yNDMctJxALrugYldkEdDTYie6r/f7dQI=";
-
-        [Test]
-        public void Whatever()
-        {
-            string original = EncryptMe;
-
-            var key = Convert.FromBase64String(TestKey);
-            var iv = Convert.FromBase64String(TestIv);
-
-            // Encrypt the string to an array of bytes.
-            byte[] encrypted = EncryptStringToBytes_Aes(original, key, iv);
-
-            // Decrypt the bytes to a string.
-            string roundtrip = DecryptStringFromBytes_Aes(encrypted, key, iv);
-
-            roundtrip.ShouldBe(original);
-
-        }
-        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
-        {
-            // Check arguments.
-            if (plainText == null || plainText.Length <= 0)
-                throw new ArgumentNullException("plainText");
-            if (Key == null || Key.Length <= 0)
-                throw new ArgumentNullException("Key");
-            if (IV == null || IV.Length <= 0)
-                throw new ArgumentNullException("IV");
-            byte[] encrypted;
-
-            // Create an Aes object
-            // with the specified key and IV.
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = Key;
-                aesAlg.IV = IV;
-
-                // Create an encryptor to perform the stream transform.
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for encryption.
-                using (MemoryStream msEncrypt = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            //Write all data to the stream.
-                            swEncrypt.Write(plainText);
-                        }
-                        encrypted = msEncrypt.ToArray();
-                    }
-                }
-            }
-
-            // Return the encrypted bytes from the memory stream.
-            return encrypted;
-        }
-
-        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
-        {
-            // Check arguments.
-            if (cipherText == null || cipherText.Length <= 0)
-                throw new ArgumentNullException("cipherText");
-            if (Key == null || Key.Length <= 0)
-                throw new ArgumentNullException("Key");
-            if (IV == null || IV.Length <= 0)
-                throw new ArgumentNullException("IV");
-
-            // Declare the string used to hold
-            // the decrypted text.
-            string plaintext = null;
-
-            // Create an Aes object
-            // with the specified key and IV.
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = Key;
-                aesAlg.IV = IV;
-
-                // Create a decryptor to perform the stream transform.
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for decryption.
-                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
-                {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                        {
-
-                            // Read the decrypted bytes from the decrypting stream
-                            // and place them in a string.
-                            plaintext = srDecrypt.ReadToEnd();
-                        }
-                    }
-                }
-            }
-
-            return plaintext;
-        }
-
-
 
         [TestFixture]
         public class WhenInitializingTheService
@@ -185,17 +84,13 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Services
         [TestFixture]
         public class WhenEncryptingAString
         {
-
             [TestFixture]
             public class GivenNullInput
             {
                 [Test]
                 public void ThenThrowException()
                 {
-                    Action act = () =>
-                    {
-                        new AESEncryptorService(TestKey, TestIv).Encrypt(null);
-                    };
+                    Action act = () => { new AESEncryptorService(TestKey, TestIv).Encrypt(null); };
 
                     act.ShouldThrow<ArgumentNullException>();
                 }
@@ -241,7 +136,8 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Services
                 {
                     Action act = () =>
                     {
-                        new AESEncryptorService(TestKey, TestIv).TryDecrypt("     ", out var decryptedValue);
+                        new AESEncryptorService(TestKey, TestIv).TryDecrypt(
+                            "     ", out var decryptedValue);
                     };
 
                     act.ShouldThrow<ArgumentException>();
@@ -294,6 +190,7 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Services
                         Action act = () =>
                         {
                             new AESEncryptorService(TestKey, TestIv).TryDecrypt(
+
                                 // missing the padding for base 64
                                 "aW52YWxpZCBzaWduYXR1cmU.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
                                 out var decryptedValue);
@@ -329,7 +226,8 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Services
                     [TestCase(EncryptMe, EncryptMeEncrypted)]
                     public void ThenItShouldEncryptTheValueAndAppendSignature(string input, string encrypted)
                     {
-                        var result = new AESEncryptorService(TestKey).TryDecrypt(encrypted, out var decrypted);
+                        var result = new AESEncryptorService(TestKey).TryDecrypt(
+                            encrypted, out var decrypted);
 
                         result.ShouldBeTrue();
                         decrypted.ShouldBe(input);
--- Application/EdFi.Ods.AdminApp.Management/Services/AESEncryptorService.cs
+++ Application/EdFi.Ods.AdminApp.Management/Services/AESEncryptorService.cs
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
 using System;
 using System.IO;
 using System.Security.Cryptography;
@@ -8,8 +13,8 @@ namespace EdFi.Ods.AdminApp.Management.Services
     // ReSharper disable once InconsistentNaming - AES is proper, not Aes
     public class AESEncryptorService : IStringEncryptorService
     {
+        private readonly byte[] _initializationVector;
         private readonly byte[] _key;
-        private readonly string _initializationVector;
 
         // ReSharper disable once InconsistentNaming - AES is proper, not Aes
         public AESEncryptorService(string key, string initializationVector = null)
@@ -17,23 +22,31 @@ namespace EdFi.Ods.AdminApp.Management.Services
             if (key == null)
             {
                 throw new ArgumentNullException(
- $"Parameter {nameof(key)} cannot be null.", nameof(key));
+                    $"Parameter {nameof(key)} cannot be null.", nameof(key));
             }
 
             if (key.Trim().Length == 0)
             {
-                throw new ArgumentException($"Parameter {nameof(key)} cannot be an empty string.", nameof(key));
+                throw new ArgumentException(
+                    $"Parameter {nameof(key)} cannot be an empty string.", nameof(key));
             }
 
             _key = Convert.FromBase64String(key);
 
             // Initialization cannot be an empty string, but null is allowed. Null will be replaced with a random value.
-            if (initializationVector?.Trim().Length == 0)
+            if (initializationVector == null)
             {
-                throw new ArgumentException($"Parameter {nameof(initializationVector)} cannot be an empty string.", nameof(initializationVector));
+                return;
             }
 
-            _initializationVector = initializationVector;
+            if (initializationVector.Trim().Length == 0)
+            {
+                throw new ArgumentException(
+                    $"Parameter {nameof(initializationVector)} cannot be an empty string.",
+                    nameof(initializationVector));
+            }
+
+            _initializationVector = Convert.FromBase64String(initializationVector);
         }
 
         public string Encrypt(string value)
@@ -45,37 +58,50 @@ namespace EdFi.Ods.AdminApp.Management.Services
 
             // Do not extract this to a separate function unless you want to deal
             // with disposal of the object properly.
-            using var aes = Aes.Create();
-            if (aes == null)
+            using var encryptor = Aes.Create();
+
+            if (encryptor == null)
             {
                 throw new InvalidOperationException("Creation of an AES encryption object failed.");
             }
 
-            aes.Key = _key;
-
-            var iv = _initializationVector;
-
-            if (iv != null)
-            {
-                aes.IV = Convert.FromBase64String(iv);
-            }
-            else
-            {
-                aes.GenerateIV();
-                iv = Convert.ToBase64String(aes.IV);
-            }
-            
-            using var memStream = new MemoryStream();
-            using (var cryptoStream = new CryptoStream(memStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
-            {
-                var b = Encoding.Unicode.GetBytes(value);
-                cryptoStream.Write(b);
-            }
-
-            var encrypted = Convert.ToBase64String(memStream.ToArray());
+            var iv = Convert.ToBase64String(SetupKeyAndIV(encryptor));
+            var encrypted = EncryptValue(encryptor);
             var signature = CalculateHashValue(encrypted);
 
             return $"{iv}.{encrypted}.{signature}";
+
+            byte[] SetupKeyAndIV(Aes aes)
+            {
+                aes.Key = _key;
+                var vector = _initializationVector;
+
+                if (vector != null)
+                {
+                    aes.IV = vector;
+                }
+                else
+                {
+                    aes.GenerateIV();
+                    vector = aes.IV;
+                }
+
+                return vector;
+            }
+
+            string EncryptValue(Aes aes)
+            {
+                using var memStream = new MemoryStream();
+
+                using (var cryptoStream = new CryptoStream(
+                    memStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    var b = Encoding.Unicode.GetBytes(value);
+                    cryptoStream.Write(b);
+                }
+
+                return Convert.ToBase64String(memStream.ToArray());
+            }
         }
 
         public bool TryDecrypt(string value, out string decryptedValue)
@@ -91,50 +117,61 @@ namespace EdFi.Ods.AdminApp.Management.Services
                     $"Parameter {nameof(value)} cannot be an empty string.", nameof(value));
             }
 
-            var split = value.Split('.');
+            var components = ExtractComponents(value);
+            ValidateSignature(components);
+            decryptedValue = Decrypt(components);
 
-            if (split.Length != 3)
+            return true;
+
+            // TODO: think about what conditions would cause False
+            // Probably just try catch and return false
+            // Must carefully detect Key or IV errors so that we can throw a proper exception rather than returning false
+
+            (string iv, string encrypted, string signature) ExtractComponents(string localValue)
             {
-                throw new InvalidOperationException(
-                    "Unable to decrypt the string because it is not signed properly.");
+                var split = localValue.Split('.');
+
+                if (split.Length != 3)
+                {
+                    throw new InvalidOperationException(
+                        "Unable to decrypt the string because it is not signed properly.");
+                }
+
+                return (split[0], split[1], split[2]);
             }
 
-            var iv = split[0];
-            var encrypted = split[1];
-            var signature = split[2];
-
-            if (CalculateHashValue(encrypted) != signature)
+            void ValidateSignature((string iv, string encrypted, string signature) localComp)
             {
-                throw new InvalidOperationException("Signatures do not match.");
+                if (CalculateHashValue(localComp.encrypted) != localComp.signature)
+                {
+                    throw new InvalidOperationException("Signatures do not match.");
+                }
             }
 
-            using (var aes = Aes.Create())
+            string Decrypt((string iv, string encrypted, string signature) localComp)
             {
+                using var aes = Aes.Create();
+
                 if (aes == null)
                 {
                     throw new InvalidOperationException("Creation of an AES encryption object failed.");
                 }
 
                 aes.Key = _key;
-                aes.IV = Convert.FromBase64String(iv);
-                
-                var b = Convert.FromBase64String(encrypted);
+                aes.IV = Convert.FromBase64String(components.iv);
 
-                using (var memStream = new MemoryStream())
+                var b = Convert.FromBase64String(localComp.encrypted);
+
+                using var memStream = new MemoryStream();
+
+                using (var cryptoStream = new CryptoStream(
+                    memStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
                 {
-
-                    using (var cryptoStream = new CryptoStream(memStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
-                    {
-                        cryptoStream.Write(b, 0, b.Length);
-                    }
-
-                    decryptedValue = Encoding.Unicode.GetString(memStream.ToArray());
+                    cryptoStream.Write(b, 0, b.Length);
                 }
+
+                return Encoding.Unicode.GetString(memStream.ToArray());
             }
-
-            return true;
-
-            // TODO: think about what conditions would cause False 
         }
 
         private string CalculateHashValue(string value)

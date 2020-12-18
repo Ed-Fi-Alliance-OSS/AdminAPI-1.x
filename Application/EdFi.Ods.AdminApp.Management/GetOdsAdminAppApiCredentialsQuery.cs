6e4f0748d3717613866d3ecd5e4f107f9c840511
Stephen A. Fuqua <stephen@safnet.com>
2020-12-18 08:38:05 -0600

    Resolve null reference error on first time startup

--- Application/EdFi.Ods.AdminApp.Management/GetOdsAdminAppApiCredentialsQuery.cs
+++ Application/EdFi.Ods.AdminApp.Management/GetOdsAdminAppApiCredentialsQuery.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -21,8 +21,8 @@ namespace EdFi.Ods.AdminApp.Management
         public async Task<OdsAdminAppApiCredentials> Execute()
         {
             var configuration = await _odsSecretConfigurationProvider.GetSecretConfiguration(_instanceContext.Id);
-
-            return configuration.GetOdsAdminAppApiCredentials();
+            
+            return configuration?.GetOdsAdminAppApiCredentials();
         }
     }
 }

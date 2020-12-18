6e4fd271dedcc88a96b0c6293cd70c64d3b2ca85
Leonardo Soto <lsoto@growthaccelerationpartners.com>
2020-12-18 17:25:45 -0500

    Handle error message when claim set name is too big

--- Application/EdFi.Ods.AdminApp.Web/Models/ViewModels/ClaimSets/CopyClaimSetModel.cs
+++ Application/EdFi.Ods.AdminApp.Web/Models/ViewModels/ClaimSets/CopyClaimSetModel.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -25,6 +25,10 @@ namespace EdFi.Ods.AdminApp.Web.Models.ViewModels.ClaimSets
         {
             _context = context;
             RuleFor(m => m.Name).NotEmpty().Must(BeAUniqueName).WithMessage("The new claim set must have a unique name");
+
+            RuleFor(m => m.Name)
+                .MaximumLength(255)
+                .WithMessage("The claim set name must be maximum 255 characters.");
         }
 
         private bool BeAUniqueName(string newName)

eb7858b3e80cf98e85ab715cac58f4f33b880f1f
Patrick Lioi <patrick@headspring.com>
2021-02-04 17:04:45 -0600

    Remove ConnectionStrings.GetConnectionStringByName(...), now that it is no longer used.

--- Application/EdFi.Ods.AdminApp.Management/Helpers/ConnectionStrings.cs
+++ Application/EdFi.Ods.AdminApp.Management/Helpers/ConnectionStrings.cs
@@ -3,8 +3,6 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System;
-
 namespace EdFi.Ods.AdminApp.Management.Helpers
 {
     public class ConnectionStrings
@@ -12,20 +10,5 @@ namespace EdFi.Ods.AdminApp.Management.Helpers
         public string Admin { get; set; }
         public string Security { get; set; }
         public string ProductionOds { get; set; }
-
-        public string GetConnectionStringByName(string databaseName)
-        {
-            switch (databaseName)
-            {
-                case CloudOdsDatabaseNames.Admin:
-                    return Admin;
-                case CloudOdsDatabaseNames.Security:
-                    return Security;
-                case CloudOdsDatabaseNames.ProductionOds:
-                    return ProductionOds;
-                default:
-                    throw new Exception("Invalid Database Name provided.");
-            }
-        }
     }
 }

c595fad7624d7b33e206d7d977db44f4c173df39
Patrick Lioi <patrick@headspring.com>
2021-02-04 13:13:25 -0600

    Remove unused member from AzureDatabaseManagementService.

--- Application/EdFi.Ods.AdminApp.Management.Azure/AzureDatabaseManagementService.cs
+++ Application/EdFi.Ods.AdminApp.Management.Azure/AzureDatabaseManagementService.cs
@@ -3,22 +3,12 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System;
 using System.Data.SqlClient;
-using System.Threading;
-using EdFi.Ods.AdminApp.Management.Database;
 
 namespace EdFi.Ods.AdminApp.Management.Azure
 {
     public class AzureDatabaseManagementService
     {
-        private readonly IRawSqlConnectionService _rawSqlConnectionService;
-
-        public AzureDatabaseManagementService(IRawSqlConnectionService rawSqlConnectionService)
-        {
-           _rawSqlConnectionService = rawSqlConnectionService;
-        }
-
         public virtual AzurePerformanceLevel GetDatabasePerformanceLevel(SqlConnection connection, string databaseName)
         {
             var sql = $"SELECT DATABASEPROPERTYEX('{databaseName}', 'ServiceObjective') AS ServiceObjective, DATABASEPROPERTYEX('{databaseName}', 'Edition') AS Edition";

f99a6cad04539578b8f8dbe0539da5b0908bcf50
Patrick Lioi <patrick@headspring.com>
2021-02-05 09:08:18 -0600

    Remove unused appsetting SystemManagedSqlServer.

--- Application/EdFi.Ods.AdminApp.Web/Infrastructure/CloudOdsAdminAppSettings.cs
+++ Application/EdFi.Ods.AdminApp.Web/Infrastructure/CloudOdsAdminAppSettings.cs
@@ -36,13 +36,6 @@ namespace EdFi.Ods.AdminApp.Web.Infrastructure
         public string ApiExternalUrl => AppSettings.ApiExternalUrl;
 
         [Obsolete("This property is no longer used.")]
-        public bool SystemManagedSqlServer
-            => AppSettings.SystemManagedSqlServer == null ||
-               bool.TrueString.Equals(
-                   AppSettings.SystemManagedSqlServer,
-                   StringComparison.InvariantCultureIgnoreCase);
-
-        [Obsolete("This property is no longer used.")]
         public bool DbSetupEnabled => bool.TrueString.Equals(
             AppSettings.DbSetupEnabled, StringComparison.InvariantCultureIgnoreCase);
 

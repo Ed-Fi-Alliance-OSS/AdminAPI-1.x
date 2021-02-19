3f5cc4290ce8f6003a774f668e5d7fa519d15d33
Patrick Lioi <patrick@headspring.com>
2021-02-19 14:23:00 -0600

    GetDescriptorCategoryName(...) gains test coverage, and is resilient to unexpected variations in the original input path.

--- Application/EdFi.Ods.AdminApp.Web/Helpers/StringExtensions.cs
+++ Application/EdFi.Ods.AdminApp.Web/Helpers/StringExtensions.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -45,22 +45,25 @@ namespace EdFi.Ods.AdminApp.Web.Helpers
 
         public static string GetDescriptorCategoryName(this string descriptorPath)
         {
-            var descriptorPathParts = DescriptorPathParts(descriptorPath);
-
-            string descriptorName = null;
+            var descriptorPathParts = descriptorPath.TrimStart('/').Split('/', 2);
 
             if (descriptorPathParts.Length == 2)
             {
                 var routePrefix = descriptorPathParts[0];
                 var name = descriptorPathParts[1];
 
-                descriptorName = name.Remove(name.Length - 1, 1).CapitalizeFirstLetter();
+                if (name.EndsWith("Descriptors"))
+                    name = name.Remove(name.Length - 1, 1);
+
+                var descriptorName = name.CapitalizeFirstLetter();
 
                 if (routePrefix != "ed-fi")
                     descriptorName = $"{descriptorName} [{routePrefix.CapitalizeFirstLetter()}]";
+
+                return descriptorName;
             }
 
-            return descriptorName;
+            return descriptorPath;
         }
 
         private static string CapitalizeFirstLetter(this string text)
@@ -68,12 +71,6 @@ namespace EdFi.Ods.AdminApp.Web.Helpers
             if (text.Length > 0)
                 return char.ToUpper(text[0]) + text.Substring(1);
             return text;
-        }
-
-        private static string[] DescriptorPathParts(string descriptorPath)
-        {
-            var descriptorPathParts = descriptorPath.TrimStart('/').Split('/', 3);
-            return descriptorPathParts;
         }
     }
 }

dfa1f060518dfcc028c383c9c122051e001220b8
Saagar Minocha <saagar.minocha93@gmail.com>
2021-02-25 14:33:43 -0600

    Upgrade Admin App version to 2.2.0 form 2.1.1 (#146)

--- .teamcity/_Self/Project.kt
+++ .teamcity/_Self/Project.kt
@@ -18,7 +18,7 @@ object AdminAppProject : Project({
             +:refs/(pull/*)/merge
         """.trimIndent())
         param("teamcity.ui.settings.readOnly","true")
-        param("adminApp.version", "2.1.1")
+        param("adminApp.version", "2.2.0")
     }
 
     template(_self.templates.BuildAndTestTemplate)

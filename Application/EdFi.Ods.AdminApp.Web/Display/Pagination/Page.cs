980fe4cadd5e17260b22eb5e25a17271f9a83459
Saagar Minocha <saagar.minocha93@gmail.com>
2021-03-10 07:33:21 -0600

    Renamed Fetch to Fetch Async. Added synchronous versions of Fetch and PagedList methods to allow paging on features which don't use an async/await flow.

--- Application/EdFi.Ods.AdminApp.Web/Display/Pagination/Page.cs
+++ Application/EdFi.Ods.AdminApp.Web/Display/Pagination/Page.cs
@@ -7,18 +7,24 @@ namespace EdFi.Ods.AdminApp.Web.Display.Pagination
 {
     public class Page<T>
     {
-        private readonly Func<int, int, Task<IReadOnlyList<T>>> GetApiRecords;
+        private readonly Func<int, int, Task<IReadOnlyList<T>>> GetApiRecordsAsync;
+        private readonly Func<int, int, IReadOnlyList<T>> GetApiRecords;
 
         public static readonly int DefaultPageSize = 20;
 
-        public Page(Func<int, int, Task<IReadOnlyList<T>>> getApiRecords)
+        public Page(Func<int, int, Task<IReadOnlyList<T>>> getApiRecordsAsync)
+        {
+            GetApiRecordsAsync = getApiRecordsAsync;
+        }
+
+        public Page(Func<int, int, IReadOnlyList<T>> getApiRecords)
         {
             GetApiRecords = getApiRecords;
         }
 
-        public static async Task<PagedList<T>> Fetch(Func<int, int, Task<IReadOnlyList<T>>> getApiRecords, int pageNumber) => await Fetch(getApiRecords, pageNumber, DefaultPageSize);
+        public static async Task<PagedList<T>> FetchAsync(Func<int, int, Task<IReadOnlyList<T>>> getApiRecords, int pageNumber) => await FetchAsync(getApiRecords, pageNumber, DefaultPageSize);
 
-        public static async Task<PagedList<T>> Fetch(Func<int, int, Task<IReadOnlyList<T>>> getApiRecords, int pageNumber, int pageSize)
+        public static async Task<PagedList<T>> FetchAsync(Func<int, int, Task<IReadOnlyList<T>>> getApiRecords, int pageNumber, int pageSize)
         {
             var service = new Page<T>(getApiRecords);
 
@@ -36,7 +42,40 @@ namespace EdFi.Ods.AdminApp.Web.Display.Pagination
 
             var recordsToOffset = (humanPageNumber - 1) * pageSize;
 
-            var records = await GetApiRecords(recordsToOffset, pageSize + 1);
+            var records = await GetApiRecordsAsync(recordsToOffset, pageSize + 1);
+
+            return new PagedList<T>
+            {
+                PageNumber = humanPageNumber,
+                NextPageHasResults = records.Count() > pageSize,
+                Items = records.Take(pageSize).ToList()
+            };
+        }
+
+        public static PagedList<T> Fetch(Func<int, int, IReadOnlyList<T>> getApiRecords, int pageNumber)
+        {
+            return Fetch(getApiRecords, pageNumber, DefaultPageSize);
+        }
+
+        public static PagedList<T> Fetch(Func<int, int, IReadOnlyList<T>> getApiRecords, int pageNumber, int pageSize)
+        {
+            var service = new Page<T>(getApiRecords);
+
+            return service.PagedList(pageNumber, pageSize);
+        }
+
+        private PagedList<T> PagedList(int pageNumber, int pageSize)
+        {
+            if (pageSize >= 100)
+            {
+                throw new ArgumentException("Page numbers of 100 or greater are not currently supported due to limit constraints on the EdFi API");
+            }
+
+            var humanPageNumber = pageNumber == 0 ? 1 : pageNumber;
+
+            var recordsToOffset = (humanPageNumber - 1) * pageSize;
+
+            var records = GetApiRecords(recordsToOffset, pageSize + 1);
 
             return new PagedList<T>
             {

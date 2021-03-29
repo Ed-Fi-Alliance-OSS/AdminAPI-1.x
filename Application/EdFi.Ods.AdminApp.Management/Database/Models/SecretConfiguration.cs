c78b091e9bdebffb3b034d0ed82f7f560f045c67
Suganya <suganya.rajendran@instructure.com>
2021-03-29 16:55:44 -0500

    Adding new column IsEncrypted to AzureSqlConfiguration and SecretConfiguration

--- Application/EdFi.Ods.AdminApp.Management/Database/Models/SecretConfiguration.cs
+++ Application/EdFi.Ods.AdminApp.Management/Database/Models/SecretConfiguration.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -9,5 +9,6 @@ namespace EdFi.Ods.AdminApp.Management.Database.Models
     {
         public string EncryptedData { get; set; }
         public int? OdsInstanceRegistrationId { get; set; }
+        public bool IsEncrypted { get; set; }
     }
 }

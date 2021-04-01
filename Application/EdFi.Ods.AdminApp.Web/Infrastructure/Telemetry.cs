2e414bdcbca1c25775a6db94f395682bec4cff65
Saagar Minocha <saagar.minocha93@gmail.com>
2021-04-01 13:58:09 -0500

    Moved Telemetry.cs to Web project. Added custom dimensions userName, databaseVersion, odsApiVersion with a cache component to it.

--- Application/EdFi.Ods.AdminApp.Web/Infrastructure/Telemetry.cs
+++ Application/EdFi.Ods.AdminApp.Web/Infrastructure/Telemetry.cs
@@ -1,12 +1,17 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using EdFi.Ods.AdminApp.Management;
 using EdFi.Ods.AdminApp.Management.Api;
+using EdFi.Ods.AdminApp.Management.Database;
 using EdFi.Ods.AdminApp.Management.Helpers;
 using GoogleAnalyticsTracker.Simple;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using static System.Environment;
 
-namespace EdFi.Ods.AdminApp.Management
+namespace EdFi.Ods.AdminApp.Web.Infrastructure
 {
     public interface ITelemetry
     {
@@ -15,19 +20,23 @@ namespace EdFi.Ods.AdminApp.Management
 
     public class Telemetry : ITelemetry
     {
+        private readonly AdminAppUserContext _userContext;
+        private readonly AdminAppDbContext _database;
         private readonly AppSettings _appSettings;
         private const string NotSet = "(not set)";
 
         private readonly string _measurementId;
-        private readonly string _internalVersion;
+        private readonly string _informationalVersion;
         private readonly SimpleTrackerEnvironment _environment;
 
-        public Telemetry(IOptions<AppSettings> appSettingsAccessor)
+        public Telemetry(IOptions<AppSettings> appSettingsAccessor, AdminAppUserContext userContext, AdminAppDbContext database)
         {
+            _userContext = userContext;
+            _database = database;
             _appSettings = appSettingsAccessor.Value;
             _measurementId = _appSettings.GoogleAnalyticsMeasurementId;
 
-            _internalVersion = Version.InformationalVersion;
+            _informationalVersion = Version.InformationalVersion;
 
             _environment = new SimpleTrackerEnvironment(
                 OSVersion.Platform.ToString(),
@@ -39,12 +48,21 @@ namespace EdFi.Ods.AdminApp.Management
         public async Task Event(string category, string action, string label)
         {
             //NOTE: Custom Dimension numbers are meaningful, but defined within Google Analytics.
-            var apiVersion = new InferOdsApiVersion().Version(_appSettings.ProductionApiUrl);
+            var odsApiVersion = InMemoryCache.Instance
+                .GetOrSet("OdsApiVersion", () => new InferOdsApiVersion().Version(CloudOdsAdminAppSettings.Instance.ProductionApiUrl));
+
+            var databaseVersion = InMemoryCache.Instance
+                .GetOrSet("DatabaseVersion", DatabaseVersion);
+
+            var userName = InMemoryCache.Instance
+                .GetOrSet("UserName", () => _userContext.User.UserName);
 
             var customDimensions = new Dictionary<int, string>
             {
-                [1] = ExplicitWhenNotSet(_internalVersion),
-                [2] = ExplicitWhenNotSet(apiVersion)
+                [1] = ExplicitWhenNotSet(_informationalVersion),
+                [2] = ExplicitWhenNotSet(odsApiVersion),
+                [3] = ExplicitWhenNotSet(databaseVersion),
+                [4] = ExplicitWhenNotSet(userName)
             };
 
             using (var tracker = new SimpleTracker(_measurementId, _environment))
@@ -53,6 +71,13 @@ namespace EdFi.Ods.AdminApp.Management
                     ExplicitWhenNotSet(action),
                     ExplicitWhenNotSet(label),
                     customDimensions);
+
+            string DatabaseVersion()
+            {
+                return "SqlServer".Equals(CloudOdsAdminAppSettings.Instance.DatabaseEngine, StringComparison.InvariantCultureIgnoreCase)
+                    ? _database.DatabaseVersionView.FromSqlRaw("SELECT @@VERSION as VersionString").First().VersionString
+                    : _database.DatabaseVersionView.FromSqlRaw("SELECT version() as VersionString").First().VersionString;
+            }
         }
 
         private static string ExplicitWhenNotSet(string customDimensionValue)

445358db277b7047467db6dd25cd1b8e679bfcb2
Saagar Minocha <saagar.minocha93@gmail.com>
2021-06-30 15:54:20 -0500

    Added GetPostSecondaryInstitutionLevels, GetAdministrativeFundingControls, GetAllPostSecondaryInstitutions, GetPostSecondaryInstitutionsByPage, AddPostSecondaryInstitution, GetPostSecondaryInstitutionById, EditPostSecondaryInstitution, DeletePostSecondaryInstitution, GetAccreditationStatusDescriptors, GetFederalLocaleCodeDescriptors, GetAllPsiSchools, AddPsiSchool methods to OdsApiFacade.

--- Application/EdFi.Ods.AdminApp.Management/Api/IOdsApiFacade.cs
+++ Application/EdFi.Ods.AdminApp.Management/Api/IOdsApiFacade.cs
@@ -11,12 +11,14 @@ namespace EdFi.Ods.AdminApp.Management.Api
     public interface IOdsApiFacade
     {
         List<School> GetAllSchools();
+        List<PsiSchool> GetAllPsiSchools();
         List<LocalEducationAgency> GetAllLocalEducationAgencies();
         List<LocalEducationAgency> GetLocalEducationAgenciesByPage(int offset = 0, int limit = 50);
         OdsApiResult DeleteLocalEducationAgency(string id);
         OdsApiResult DeleteSchool(string id);
         OdsApiResult AddLocalEducationAgency(LocalEducationAgency newLocalEducationAgency);
         OdsApiResult AddSchool(School newSchool);
+        OdsApiResult AddPsiSchool(PsiSchool newSchool);
         List<SelectOptionModel> GetLocalEducationAgencyCategories();
         List<SelectOptionModel> GetAllGradeLevels();
         IReadOnlyList<string> GetAllDescriptors();
@@ -28,5 +30,15 @@ namespace EdFi.Ods.AdminApp.Management.Api
         School GetSchoolById(string id);
         OdsApiResult EditSchool(School editedSchool);
         List<SelectOptionModel> GetAllStateAbbreviations();
+        List<SelectOptionModel> GetPostSecondaryInstitutionLevels();
+        List<SelectOptionModel> GetAdministrativeFundingControls();
+        List<SelectOptionModel> GetAccreditationStatusOptions();
+        List<SelectOptionModel> GetFederalLocaleCodes();
+        List<PostSecondaryInstitution> GetAllPostSecondaryInstitutions();
+        List<PostSecondaryInstitution> GetPostSecondaryInstitutionsByPage(int offset = 0, int limit = 50);
+        OdsApiResult AddPostSecondaryInstitution(Models.PostSecondaryInstitution newPostSecondaryInstitution);
+        PostSecondaryInstitution GetPostSecondaryInstitutionById(string id);
+        OdsApiResult EditPostSecondaryInstitution(Models.PostSecondaryInstitution model);
+        OdsApiResult DeletePostSecondaryInstitution(string id);
     }
 }

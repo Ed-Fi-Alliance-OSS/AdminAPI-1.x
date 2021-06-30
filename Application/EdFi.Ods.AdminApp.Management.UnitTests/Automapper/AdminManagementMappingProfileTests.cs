300c23a6433a72501e0dc0937a7ef3d914e5130e
Saagar Minocha <saagar.minocha93@gmail.com>
2021-06-30 15:45:28 -0500

    Refactored and added mappings related to PSI models. Updated and added unit tests to match the new mappings.

--- Application/EdFi.Ods.AdminApp.Management.UnitTests/Automapper/AdminManagementMappingProfileTests.cs
+++ Application/EdFi.Ods.AdminApp.Management.UnitTests/Automapper/AdminManagementMappingProfileTests.cs
@@ -43,6 +43,7 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Automapper
             private const string Name = "name";
             private const int SchoolId = 1000;
             private const int LeaId = 1234;
+            private const int PsiId = 12345;
             private const string AddressTypeDescriptor = "Namespace#Physical";
             private const string StreetNumberName = "StreetNumberName";
             private const string ApartmentRoomSuiteNumber = "206";
@@ -53,11 +54,18 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Automapper
             private const string GradeLevelDescriptor = "Namespace#Kinder";
             private const string LocalEducationAgencyCategoryDescriptor = "NameSpace#Private";
             private const string LocalEducationAgencyCategoryType = "Public";
+            private const string PostSecondaryInstitutionCategoryType = "PSI";
+            private const string PostSecondaryInstitutionLevel = "Public";
+            private const string AdministrativeFundingControl = "Public";
+            private const string AccreditationStatusDescriptor = "TestAccreditationStatus";
+            private const string FederalLocaleCodeDescriptor = "TestFederalLocaleCode";
+            private const bool ImprovingSchool = false;
             private const int StateEducationAgencyId = 999;
             private List<EdFiEducationOrganizationAddress> _addresses;
             private List<EdFiSchoolGradeLevel> _gradeLevels;
             private List<EdFiEducationOrganizationCategory> _edOrgCategories;
             private EdFiLocalEducationAgencyReference _leaReference;
+            private SchoolExtensions _schoolExtensions;
             private EdFiStateEducationAgencyReference _stateEducationAgencyReference;
 
             [SetUp]
@@ -77,6 +85,11 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Automapper
                     {new EdFiSchoolGradeLevel(GradeLevelDescriptor)};
                 _edOrgCategories = new List<EdFiEducationOrganizationCategory>();
                 _leaReference = new EdFiLocalEducationAgencyReference(LeaId);
+
+                var postSecondaryInstitutionReference = new EdFiPostSecondaryInstitutionReference(PsiId);
+                var tpdmExtension = new TpdmSchoolExtension(AccreditationStatusDescriptor, FederalLocaleCodeDescriptor, ImprovingSchool, postSecondaryInstitutionReference);
+                _schoolExtensions = new SchoolExtensions(tpdmExtension);
+
                 _stateEducationAgencyReference = new EdFiStateEducationAgencyReference(StateEducationAgencyId);
             }
 
@@ -105,6 +118,33 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Automapper
             }
 
             [Test]
+            public void When_Mapping_EdFiPsiSchool_to_School_Should_Have_Expected_Values_On_Destination()
+            {
+                // Arrange
+                var edfiSchool = new EdFiSchool(Id, Name, SchoolId, _addresses, _edOrgCategories, _gradeLevels, null, _schoolExtensions);
+
+                // Act
+                var result = _mapper.Map<PsiSchool>(edfiSchool);
+
+                // Assert
+                result.ShouldNotBeNull();
+                result.Id.ShouldBe(Id);
+                result.Name.ShouldBe(Name);
+                result.EducationOrganizationId.ShouldBe(SchoolId);
+                result.PostSecondaryInstitutionId.ShouldBe(PsiId);
+                result.LocalEducationAgencyId.ShouldBeNull();
+                result.StreetNumberName.ShouldBe(StreetNumberName);
+                result.ApartmentRoomSuiteNumber.ShouldBe(ApartmentRoomSuiteNumber);
+                result.State.ShouldBe(StateAbbreviationDescriptor);
+                result.City.ShouldBe(City);
+                result.ZipCode.ShouldBe(PostalCode);
+                result.GradeLevels.Count.ShouldBeGreaterThan(0);
+                result.GradeLevels.First().ShouldBe(GradeLevelDescriptor);
+                result.AccreditationStatus.ShouldBe(AccreditationStatusDescriptor);
+                result.FederalLocaleCode.ShouldBe(FederalLocaleCodeDescriptor);
+            }
+
+            [Test]
             public void When_Mapping_EdFiLocalEducationAgency_to_LocalEducationAgency_Should_Have_Expected_Values_On_Destination()
             {
                 // Arrange
@@ -128,6 +168,33 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Automapper
                 result.ZipCode.ShouldBe(PostalCode);
                 result.LocalEducationAgencyCategoryType.ShouldBe(LocalEducationAgencyCategoryDescriptor);
                 result.StateOrganizationId.ShouldBe(StateEducationAgencyId);
+            }
+
+            [Test]
+            public void When_Mapping_EdFiPostSecondaryInstitution_to_PostSecondaryInstitution_Should_Have_Expected_Values_On_Destination()
+            {
+                // Arrange
+                var edfiPsi = new EdFiPostSecondaryInstitution(Id, _addresses, _edOrgCategories,
+                    postSecondaryInstitutionLevelDescriptor: PostSecondaryInstitutionLevel,
+                    administrativeFundingControlDescriptor: AdministrativeFundingControl,
+                    nameOfInstitution: Name, postSecondaryInstitutionId: PsiId);
+
+                // Act
+                var result = _mapper.Map<PostSecondaryInstitution>(edfiPsi);
+
+                // Assert
+                result.ShouldNotBeNull();
+                result.Id.ShouldBe(Id);
+                result.Name.ShouldBe(Name);
+                result.EducationOrganizationId.ShouldBe(PsiId);
+                result.PostSecondaryInstitutionId.ShouldBe(PsiId);
+                result.StreetNumberName.ShouldBe(StreetNumberName);
+                result.ApartmentRoomSuiteNumber.ShouldBe(ApartmentRoomSuiteNumber);
+                result.State.ShouldBe(StateAbbreviationDescriptor);
+                result.City.ShouldBe(City);
+                result.ZipCode.ShouldBe(PostalCode);
+                result.PostSecondaryInstitutionLevel.ShouldBe(PostSecondaryInstitutionLevel);
+                result.AdministrativeFundingControl.ShouldBe(AdministrativeFundingControl);
             }
 
             [Test]
@@ -168,6 +235,52 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Automapper
                 result.GradeLevels.Count.ShouldBe(1);
                 result.GradeLevels.First().GradeLevelDescriptor.ShouldBe(GradeLevelDescriptor);
                 result.LocalEducationAgencyReference.LocalEducationAgencyId.ShouldBe(LeaId);
+                result._ext.ShouldBeNull();
+            }
+
+            [Test]
+            public void When_Mapping_PsiSchool_to_EdFiSchool_Should_Have_Expected_Values_On_Destination()
+            {
+                // Arrange
+                var school = new PsiSchool
+                {
+                    Id = Id,
+                    EducationOrganizationId = SchoolId,
+                    Name = Name,
+                    State = State,
+                    ApartmentRoomSuiteNumber = ApartmentRoomSuiteNumber,
+                    StreetNumberName = StreetNumberName,
+                    City = City,
+                    ZipCode = PostalCode,
+                    GradeLevels = new List<string> { GradeLevelDescriptor },
+                    PostSecondaryInstitutionId = PsiId,
+                    AccreditationStatus = AccreditationStatusDescriptor,
+                    FederalLocaleCode = FederalLocaleCodeDescriptor,
+                    EducationOrganizationCategory = PostSecondaryInstitutionCategoryType
+                };
+
+                // Act
+                var result = _mapper.Map<EdFiSchool>(school);
+
+                // Assert
+                result.ShouldNotBeNull();
+                result.Id.ShouldBe(Id);
+                result.SchoolId.ShouldBe(SchoolId);
+                result.NameOfInstitution.ShouldBe(Name);
+                result.Addresses.Count.ShouldBe(1);
+                result.Addresses.First().StreetNumberName.ShouldBe(StreetNumberName);
+                result.Addresses.First().ApartmentRoomSuiteNumber.ShouldBe(ApartmentRoomSuiteNumber);
+                result.Addresses.First().City.ShouldBe(City);
+                result.Addresses.First().StateAbbreviationDescriptor.ShouldContain(State);
+                result.Addresses.First().PostalCode.ShouldBe(PostalCode);
+                result.EducationOrganizationCategories.Count.ShouldBe(1);
+                result.EducationOrganizationCategories.First().EducationOrganizationCategoryDescriptor.ShouldContain(PostSecondaryInstitutionCategoryType);
+                result.GradeLevels.Count.ShouldBe(1);
+                result.GradeLevels.First().GradeLevelDescriptor.ShouldBe(GradeLevelDescriptor);
+                result._ext.TPDM.PostSecondaryInstitutionReference.PostSecondaryInstitutionId.ShouldBe(PsiId);
+                result._ext.TPDM.AccreditationStatusDescriptor.ShouldBe(AccreditationStatusDescriptor);
+                result._ext.TPDM.FederalLocaleCodeDescriptor.ShouldBe(FederalLocaleCodeDescriptor);
+                result.LocalEducationAgencyReference.ShouldBeNull();
             }
 
             [Test]
@@ -207,6 +320,45 @@ namespace EdFi.Ods.AdminApp.Management.UnitTests.Automapper
                 result.StateEducationAgencyReference.StateEducationAgencyId.ShouldBe(StateEducationAgencyId);
                 result.Categories.Count.ShouldBe(1);
                 result.Categories.First().EducationOrganizationCategoryDescriptor.ShouldContain(LocalEducationAgencyCategoryType);
+            }
+
+            [Test]
+            public void When_Mapping_PostSecondaryInstitution_to_EdFiPostSecondaryInstitution_Should_Have_Expected_Values_On_Destination()
+            {
+                // Arrange
+                var psi = new PostSecondaryInstitution
+                {
+                    Id = Id,
+                    PostSecondaryInstitutionId = PsiId,
+                    Name = Name,
+                    State = State,
+                    ApartmentRoomSuiteNumber = ApartmentRoomSuiteNumber,
+                    StreetNumberName = StreetNumberName,
+                    City = City,
+                    ZipCode = PostalCode,
+                    EducationOrganizationCategory = PostSecondaryInstitutionCategoryType,
+                    PostSecondaryInstitutionLevel = PostSecondaryInstitutionLevel,
+                    AdministrativeFundingControl = AdministrativeFundingControl
+                };
+
+                // Act
+                var result = _mapper.Map<EdFiPostSecondaryInstitution>(psi);
+
+                // Assert
+                result.ShouldNotBeNull();
+                result.Id.ShouldBe(Id);
+                result.PostSecondaryInstitutionId.ShouldBe(PsiId);
+                result.NameOfInstitution.ShouldBe(Name);
+                result.PostSecondaryInstitutionLevelDescriptor.ShouldBe(PostSecondaryInstitutionLevel);
+                result.AdministrativeFundingControlDescriptor.ShouldBe(AdministrativeFundingControl);
+                result.Addresses.Count.ShouldBe(1);
+                result.Addresses.First().StreetNumberName.ShouldBe(StreetNumberName);
+                result.Addresses.First().City.ShouldBe(City);
+                result.Addresses.First().StateAbbreviationDescriptor.ShouldContain(State);
+                result.Addresses.First().ApartmentRoomSuiteNumber.ShouldBe(ApartmentRoomSuiteNumber);
+                result.Addresses.First().PostalCode.ShouldBe(PostalCode);
+                result.Categories.Count.ShouldBe(1);
+                result.Categories.First().EducationOrganizationCategoryDescriptor.ShouldContain(PostSecondaryInstitutionCategoryType);
             }
         }
     }
--- Application/EdFi.Ods.AdminApp.Management/Api/Automapper/AdminManagementMappingProfile.cs
+++ Application/EdFi.Ods.AdminApp.Management/Api/Automapper/AdminManagementMappingProfile.cs
@@ -30,6 +30,32 @@ namespace EdFi.Ods.AdminApp.Management.Api.Automapper
                 .ForMember(dst => dst.GradeLevels, opt => opt.MapFrom(src => src.GradeLevels != null  ? src.GradeLevels.Select(g => g.GradeLevelDescriptor) : new List<string>()))
                 .ForMember(dst => dst.EducationOrganizationCategory, opt => opt.Ignore()); //this value is currently stored in web.config
 
+            CreateMap<EdFiSchool, PsiSchool>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.NameOfInstitution))
+                .ForMember(dst => dst.EducationOrganizationId, opt => opt.MapFrom(src => src.SchoolId))
+                .ForMember(dst => dst.LocalEducationAgencyId, opt => opt.Ignore())
+                .ForMember(dst => dst.PostSecondaryInstitutionId, opt => opt.MapFrom(src => src._ext == null
+                    ? null : src._ext != null && src._ext.TPDM != null && src._ext.TPDM.PostSecondaryInstitutionReference == null ? null : src._ext.TPDM.PostSecondaryInstitutionReference.PostSecondaryInstitutionId))
+                .ForMember(dst => dst.StreetNumberName, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().StreetNumberName : null))
+                .ForMember(dst => dst.ApartmentRoomSuiteNumber, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().ApartmentRoomSuiteNumber : null))
+                .ForMember(dst => dst.State, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().StateAbbreviationDescriptor : null))
+                .ForMember(dst => dst.City, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().City : null))
+                .ForMember(dst => dst.ZipCode, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().PostalCode : null))
+                .ForMember(dst => dst.GradeLevels, opt => opt.MapFrom(src => src.GradeLevels != null ? src.GradeLevels.Select(g => g.GradeLevelDescriptor) : new List<string>()))
+                .ForMember(dst => dst.AccreditationStatus, opt => opt.MapFrom(src => src._ext == null
+                    ? null
+                    : src._ext != null && src._ext.TPDM == null
+                        ? null
+                        : src._ext.TPDM.AccreditationStatusDescriptor))
+                .ForMember(dst => dst.FederalLocaleCode, opt => opt.MapFrom(src => src._ext == null
+                    ? null
+                    : src._ext != null && src._ext.TPDM == null
+                        ? null
+                        : src._ext.TPDM.FederalLocaleCodeDescriptor))
+                .ForMember(dst => dst.ImprovingSchool, opt => opt.Ignore())
+                .ForMember(dst => dst.EducationOrganizationCategory, opt => opt.Ignore()); //this value is currently stored in web.config
+
             CreateMap<EdFiLocalEducationAgency, LocalEducationAgency>()
                 .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dst => dst.LocalEducationAgencyId, opt => opt.MapFrom(src => src.LocalEducationAgencyId))
@@ -44,6 +70,21 @@ namespace EdFi.Ods.AdminApp.Management.Api.Automapper
                 .ForMember(dst => dst.EducationOrganizationCategory, opt => opt.Ignore()) //this value is currently stored in web.config
                 .ForMember(dst => dst.StateOrganizationId, opt => opt.MapFrom(src => src.StateEducationAgencyReference != null ? src.StateEducationAgencyReference.StateEducationAgencyId : 0));
 
+            CreateMap<EdFiPostSecondaryInstitution, PostSecondaryInstitution>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.PostSecondaryInstitutionId, opt => opt.MapFrom(src => src.PostSecondaryInstitutionId))
+                .ForMember(dst => dst.LocalEducationAgencyId, opt => opt.Ignore())
+                .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.NameOfInstitution))
+                .ForMember(dst => dst.EducationOrganizationId, opt => opt.MapFrom(src => src.PostSecondaryInstitutionId ?? 0))
+                .ForMember(dst => dst.StreetNumberName, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().StreetNumberName : null))
+                .ForMember(dst => dst.ApartmentRoomSuiteNumber, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().ApartmentRoomSuiteNumber : null))
+                .ForMember(dst => dst.State, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().StateAbbreviationDescriptor : null))
+                .ForMember(dst => dst.City, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().City : null))
+                .ForMember(dst => dst.ZipCode, opt => opt.MapFrom(src => src.Addresses != null && src.Addresses.Any() ? src.Addresses.First().PostalCode : null))
+                .ForMember(dst => dst.PostSecondaryInstitutionLevel, opt => opt.MapFrom(src => src.PostSecondaryInstitutionLevelDescriptor))
+                .ForMember(dst => dst.AdministrativeFundingControl, opt => opt.MapFrom(src => src.AdministrativeFundingControlDescriptor))
+                .ForMember(dst => dst.EducationOrganizationCategory, opt => opt.Ignore()); //this value is currently stored in web.config
+
             CreateMap<School, EdFiSchool>()
                 .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dst => dst.SchoolId, opt => opt.MapFrom(src => src.EducationOrganizationId))
@@ -54,12 +95,33 @@ namespace EdFi.Ods.AdminApp.Management.Api.Automapper
                 .ForMember(dst => dst.GradeLevels, opt => opt.MapFrom<SchoolGradeLevelResolver>())
                 .ForMember(dst => dst.LocalEducationAgencyReference,
                     opt => opt.MapFrom<LocalEducationAgencyReferenceResolver>())
+                .ForMember(dst => dst._ext, opt => opt.Ignore())
                 .ForCtorParam("id", opt => opt.MapFrom(src => src.Id))
                 .ForCtorParam("schoolId", opt => opt.MapFrom(src => src.EducationOrganizationId))
                 .ForCtorParam("nameOfInstitution", opt => opt.MapFrom(src => src.Name))
                 .ForCtorParam("addresses", opt => opt.MapFrom(AddressResolver))
                 .ForCtorParam("educationOrganizationCategories", opt => opt.MapFrom(EducationOrganizationCategoryResolver.Resolve))
                 .ForCtorParam("gradeLevels", opt => opt.MapFrom(SchoolGradeLevelResolver.Resolve));
+
+            CreateMap<PsiSchool, EdFiSchool>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.SchoolId, opt => opt.MapFrom(src => src.EducationOrganizationId))
+                .ForMember(dst => dst.NameOfInstitution, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dst => dst.Addresses, opt => opt.MapFrom<SchoolAddressResolver>())
+                .ForMember(dst => dst.EducationOrganizationCategories,
+                    opt => opt.MapFrom<SchoolCategoryResolver>())
+                .ForMember(dst => dst.GradeLevels, opt => opt.MapFrom<SchoolGradeLevelResolver>())
+                .ForMember(dst => dst.LocalEducationAgencyReference,
+                    opt => opt.MapFrom<LocalEducationAgencyReferenceResolver>())
+                .ForMember(dst => dst._ext,
+                    opt => opt.MapFrom<SchoolExtensionsResolver>())
+                .ForCtorParam("id", opt => opt.MapFrom(src => src.Id))
+                .ForCtorParam("schoolId", opt => opt.MapFrom(src => src.EducationOrganizationId))
+                .ForCtorParam("nameOfInstitution", opt => opt.MapFrom(src => src.Name))
+                .ForCtorParam("addresses", opt => opt.MapFrom(AddressResolver))
+                .ForCtorParam("educationOrganizationCategories", opt => opt.MapFrom(EducationOrganizationCategoryResolver.Resolve))
+                .ForCtorParam("gradeLevels", opt => opt.MapFrom(SchoolGradeLevelResolver.Resolve));
+
 
             CreateMap<LocalEducationAgency, EdFiLocalEducationAgency>()
                 .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
@@ -74,6 +136,22 @@ namespace EdFi.Ods.AdminApp.Management.Api.Automapper
                 .ForCtorParam("categories", opt => opt.MapFrom(EducationOrganizationCategoryResolver.Resolve))
                 .ForCtorParam("localEducationAgencyId", opt => opt.MapFrom(src => src.LocalEducationAgencyId))
                 .ForCtorParam("localEducationAgencyCategoryDescriptor", opt => opt.MapFrom(src => src.LocalEducationAgencyCategoryType))
+                .ForCtorParam("nameOfInstitution", opt => opt.MapFrom(src => src.Name));
+
+            CreateMap<PostSecondaryInstitution, EdFiPostSecondaryInstitution>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.PostSecondaryInstitutionId, opt => opt.MapFrom(src => src.PostSecondaryInstitutionId))
+                .ForMember(dst => dst.NameOfInstitution, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dst => dst.PostSecondaryInstitutionLevelDescriptor, opt => opt.MapFrom(src => src.PostSecondaryInstitutionLevel))
+                .ForMember(dst => dst.AdministrativeFundingControlDescriptor, opt => opt.MapFrom(src => src.AdministrativeFundingControl))
+                .ForMember(dst => dst.Addresses, opt => opt.MapFrom<PostSecondaryInstitutionAddressResolver>())
+                .ForMember(dst => dst.Categories, opt => opt.MapFrom<PostSecondaryInstitutionCategoryResolver>())
+                .ForCtorParam("id", opt => opt.MapFrom(src => src.Id))
+                .ForCtorParam("addresses", opt => opt.MapFrom(AddressResolver))
+                .ForCtorParam("categories", opt => opt.MapFrom(EducationOrganizationCategoryResolver.Resolve))
+                .ForCtorParam("postSecondaryInstitutionId", opt => opt.MapFrom(src => src.PostSecondaryInstitutionId))
+                .ForCtorParam("postSecondaryInstitutionLevelDescriptor", opt => opt.MapFrom(src => src.PostSecondaryInstitutionLevel))
+                .ForCtorParam("administrativeFundingControlDescriptor", opt => opt.MapFrom(src => src.AdministrativeFundingControl))
                 .ForCtorParam("nameOfInstitution", opt => opt.MapFrom(src => src.Name));
 
             List<EdFiEducationOrganizationAddress> AddressResolver(EducationOrganization source,

b7353f8817b9dacffa6988264e342ffeda49d09d
Saagar Minocha <saagar.minocha93@gmail.com>
2021-07-14 15:26:44 -0500

    Admin App version bump to 2.3.0 after 2.2.1 release (#188)

--- .teamcity/_Self/Project.kt
+++ .teamcity/_Self/Project.kt
@@ -18,7 +18,7 @@ object AdminAppProject : Project({
             +:refs/(pull/*)/merge
         """.trimIndent())
         param("teamcity.ui.settings.readOnly","true")
-        param("adminApp.version", "2.2.1")
+        param("adminApp.version", "2.3.0")
     }
 
     subProject(web.AdminAppWebProject)

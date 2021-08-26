e3b92f512220045735999b6ff00d9f326541c788
Patrick Lioi <patrick@headspring.com>
2021-08-26 16:47:34 -0500

    Tests pass regardless of the local git config line ending settings.

--- Application/EdFi.Ods.AdminApp.Management.Tests/Infrastructure/ProductRegistrationModelTests.cs
+++ Application/EdFi.Ods.AdminApp.Management.Tests/Infrastructure/ProductRegistrationModelTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using EdFi.Ods.AdminApp.Web.Infrastructure;
 using NUnit.Framework;
 using Shouldly;
@@ -42,23 +43,27 @@ namespace EdFi.Ods.AdminApp.Management.Tests.Infrastructure
                 UtcTimeStamp = new DateTime(2021, 8, 23, 21, 33,59, 268, DateTimeKind.Utc)
             };
 
-            model.Serialize().ShouldBe(@"{
-  ""ProductRegistrationId"": ""0031cc17-0fe7-4a98-92d8-179065ff35ea"",
-  ""OdsApiConnections"": [
-    {
-      ""OdsApiVersion"": ""5.2"",
-      ""OdsApiMode"": ""YearSpecific""
-    }
-  ],
-  ""ProductType"": ""Admin App"",
-  ""ProductVersion"": ""2.2.1"",
-  ""OSVersion"": ""Windows 95"",
-  ""DatabaseVersion"": ""Microsoft SQL Server 2019 (RTM-GDR) (KB4583458) - 15.0.2080.9 (X64)   Nov  6 2020 16:50:01   Copyright (C) 2019 Microsoft Corporation  Developer Edition (64-bit) on Windows 10 Pro 10.0 <X64> (Build 19042: ) "",
-  ""HostName"": ""the-installed-admin-app-hostname.example.com"",
-  ""UserId"": ""347f41d2-2e4b-45c3-b757-44d9545dba4b"",
-  ""UserName"": ""the-logged-in-user@example.com"",
-  ""UtcTimeStamp"": ""2021-08-23T21:33:59.268Z""
-}");
+            var expected = new StringBuilder()
+                    .AppendLine(@"{")
+                    .AppendLine(@"  ""ProductRegistrationId"": ""0031cc17-0fe7-4a98-92d8-179065ff35ea"",")
+                    .AppendLine(@"  ""OdsApiConnections"": [")
+                    .AppendLine(@"    {")
+                    .AppendLine(@"      ""OdsApiVersion"": ""5.2"",")
+                    .AppendLine(@"      ""OdsApiMode"": ""YearSpecific""")
+                    .AppendLine(@"    }")
+                    .AppendLine(@"  ],")
+                    .AppendLine(@"  ""ProductType"": ""Admin App"",")
+                    .AppendLine(@"  ""ProductVersion"": ""2.2.1"",")
+                    .AppendLine(@"  ""OSVersion"": ""Windows 95"",")
+                    .AppendLine(@"  ""DatabaseVersion"": ""Microsoft SQL Server 2019 (RTM-GDR) (KB4583458) - 15.0.2080.9 (X64)   Nov  6 2020 16:50:01   Copyright (C) 2019 Microsoft Corporation  Developer Edition (64-bit) on Windows 10 Pro 10.0 <X64> (Build 19042: ) "",")
+                    .AppendLine(@"  ""HostName"": ""the-installed-admin-app-hostname.example.com"",")
+                    .AppendLine(@"  ""UserId"": ""347f41d2-2e4b-45c3-b757-44d9545dba4b"",")
+                    .AppendLine(@"  ""UserName"": ""the-logged-in-user@example.com"",")
+                    .AppendLine(@"  ""UtcTimeStamp"": ""2021-08-23T21:33:59.268Z""")
+                    .Append(@"}")
+                    .ToString();
+
+             model.Serialize().ShouldBe(expected);
         }
     }
 }

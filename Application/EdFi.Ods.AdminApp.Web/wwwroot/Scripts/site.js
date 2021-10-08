466be6a1e05a7faf3db85f464047ce24529d0da4
Saagar Minocha <saagar.minocha93@gmail.com>
2021-10-08 12:55:09 -0500

    Swapped the staging-tracker issue collector script tags with the production-tracker issue collectors for Admin App 2.3+. Uncommented the fieldValues["components"] assignment to automatically select the Admin App component.

--- Application/EdFi.Ods.AdminApp.Web/wwwroot/Scripts/site.js
+++ Application/EdFi.Ods.AdminApp.Web/wwwroot/Scripts/site.js
@@ -505,15 +505,8 @@ function FieldValues() {
 
     fieldValues["summary"] = summary.length ? summary.text() : "Admin App Feedback";
 
-    /*
-     * The following should only be used for automatically selecting the Admin App component for the production issue collector form.
-     * 
-     * fieldValues["components"] = ["13314"];
-     * 
-     */
-
-    // Remove the following fieldValues assignment when ready to use the production issue-collector.
-    fieldValues["components"] = ["12702"];
+    // Selecting the Admin App component automatically
+    fieldValues["components"] = ["13314"];
 
     if (errorDescription.length) {
         var description = "\n\n\n\n ---- \n TYPE YOUR DESCRIPTION ABOVE THIS LINE \n ---- \n {code}"+errorDescription.text()+"{code}";

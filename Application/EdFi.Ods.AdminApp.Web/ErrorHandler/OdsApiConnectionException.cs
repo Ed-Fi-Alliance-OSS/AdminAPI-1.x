5ec5218008ab302214c5a26581d73f566ec35ec0
Patrick McVeety-Mill <patrickm@headspring.com>
2021-10-18 16:11:16 -0500

    introduce ods api exception

---
+++ Application/EdFi.Ods.AdminApp.Web/ErrorHandler/OdsApiConnectionException.cs
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System;
+using System.Net;
+
+namespace EdFi.Ods.AdminApp.Web.ErrorHandler
+{
+    public class OdsApiConnectionException : Exception, IAdminAppException
+    {
+        public OdsApiConnectionException(HttpStatusCode responseCode, string message) : base(message)
+        {
+            StatusCode = responseCode;
+        }
+
+        public OdsApiConnectionException(HttpStatusCode responseCode, string message, Exception innerException) : base(message, innerException)
+        {
+            StatusCode = responseCode;
+        }
+
+        public HttpStatusCode? StatusCode { get; }
+        public bool IsExceptionMessageRelevant { get => false; }
+        public bool IsStackTraceRelevant { get => false;  }
+    }
+}
+

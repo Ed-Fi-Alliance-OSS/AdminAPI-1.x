5da9f42c212468d5409001414957cc5a5adb7333
Patrick McVeety-Mill <patrickm@headspring.com>
2021-10-18 16:02:53 -0500

    add likely properties for view

--- Application/EdFi.Ods.AdminApp.Web/ErrorHandler/AdminAppException.cs
+++ Application/EdFi.Ods.AdminApp.Web/ErrorHandler/AdminAppException.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 
 namespace EdFi.Ods.AdminApp.Web.ErrorHandler
 {
@@ -7,5 +8,9 @@ namespace EdFi.Ods.AdminApp.Web.ErrorHandler
         public AdminAppException(string stackTrace, string error, string status, Exception innerException)
             : base($"Error: {error}\nStatus:{status}\nStackTrace: {stackTrace}\n", innerException)
         { }
+
+        public HttpStatusCode? StatusCode { get; set; }
+        public bool IsExceptionMessageRelevant { get; set; }
+        public bool IsStackTraceRelevant { get; set; }
     }
 }

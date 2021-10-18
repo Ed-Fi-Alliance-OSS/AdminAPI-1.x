17cfd9b92987db83a4cf745945f0dc16d2ed5fdf
Patrick McVeety-Mill <patrickm@headspring.com>
2021-10-18 16:07:31 -0500

    extract interface

--- Application/EdFi.Ods.AdminApp.Web/ErrorHandler/AdminAppException.cs
+++ Application/EdFi.Ods.AdminApp.Web/ErrorHandler/AdminAppException.cs
@@ -3,7 +3,16 @@ using System.Net;
 
 namespace EdFi.Ods.AdminApp.Web.ErrorHandler
 {
-    public class AdminAppException : Exception
+    public interface IAdminAppException
+    {
+        string Message { get; }
+        string StackTrace { get; }
+        HttpStatusCode? StatusCode { get; }
+        bool IsExceptionMessageRelevant { get; }
+        bool IsStackTraceRelevant { get; }
+    }
+
+    public class AdminAppException : Exception, IAdminAppException
     {
         public HttpStatusCode? StatusCode { get; set; }
         public bool IsExceptionMessageRelevant { get; set; }

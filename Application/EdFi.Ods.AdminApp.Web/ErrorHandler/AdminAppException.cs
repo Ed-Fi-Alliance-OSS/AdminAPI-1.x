b1f2ed936dc1cead7b65bb963ddc3e67661583a2
Patrick McVeety-Mill <patrickm@headspring.com>
2021-10-18 16:03:59 -0500

    remove unused constructor
    if we want that message we can make it later

--- Application/EdFi.Ods.AdminApp.Web/ErrorHandler/AdminAppException.cs
+++ Application/EdFi.Ods.AdminApp.Web/ErrorHandler/AdminAppException.cs
@@ -5,10 +5,6 @@ namespace EdFi.Ods.AdminApp.Web.ErrorHandler
 {
     public class AdminAppException : Exception
     {
-        public AdminAppException(string stackTrace, string error, string status, Exception innerException)
-            : base($"Error: {error}\nStatus:{status}\nStackTrace: {stackTrace}\n", innerException)
-        { }
-
         public HttpStatusCode? StatusCode { get; set; }
         public bool IsExceptionMessageRelevant { get; set; }
         public bool IsStackTraceRelevant { get; set; }

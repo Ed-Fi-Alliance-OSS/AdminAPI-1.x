d6de2b0bd7d9d0acfc5f608b7041c3f6735ddf25
Suganya <suganya.rajendran@instructure.com>
2021-11-02 11:58:10 -0500

    Remove connection provider

--- Application/EdFi.Ods.AdminApp.Web/Models/ViewModels/OdsInstances/BulkRegisterOdsInstancesModel.cs
+++ Application/EdFi.Ods.AdminApp.Web/Models/ViewModels/OdsInstances/BulkRegisterOdsInstancesModel.cs
@@ -8,7 +8,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using EdFi.Ods.AdminApp.Management.Database;
-using EdFi.Ods.AdminApp.Management.Database.Ods;
 using EdFi.Ods.AdminApp.Management.OdsInstanceServices;
 using EdFi.Ods.AdminApp.Web.Helpers;
 using EdFi.Ods.AdminApp.Web.Infrastructure;
@@ -63,8 +62,7 @@ namespace EdFi.Ods.AdminApp.Web.Models.ViewModels.OdsInstances
 
         public BulkRegisterOdsInstancesModelValidator(AdminAppDbContext database
             , ICloudOdsAdminAppSettingsApiModeProvider apiModeProvider
-            , IDatabaseValidationService databaseValidationService
-            , IDatabaseConnectionProvider databaseConnectionProvider
+            , IDatabaseValidationService databaseValidationService            
             , IBulkRegisterOdsInstancesFiltrationService dataFilterService)
         {
             var mode = apiModeProvider.GetApiMode();

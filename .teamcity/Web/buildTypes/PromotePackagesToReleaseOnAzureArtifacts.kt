6fd35890f8bc7e8e008a521ee8311fd938521bba
CSR2017 <suganya@imanisoftware.com>
2022-04-11 16:01:08 -0500

    Add "promote package to release" step

--- .teamcity/Web/buildTypes/PromotePackagesToReleaseOnAzureArtifacts.kt
+++ .teamcity/Web/buildTypes/PromotePackagesToReleaseOnAzureArtifacts.kt
@@ -10,9 +10,9 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 
-object PublishReleasePackagesToAzureArtifacts : BuildType ({
-    name = "Publish Release Packages to Azure Artifacts"
-    description = "Publishes NuGet packages to the Azure Artifacts"
+object PromotePackagesToReleaseOnAzureArtifacts : BuildType ({
+    name = "Promote Packages to Release on Azure Artifacts"
+    description = "Promote Packages to Release on Azure Artifacts"
 
     publishArtifacts = PublishMode.SUCCESSFUL
 
@@ -20,8 +20,6 @@ object PublishReleasePackagesToAzureArtifacts : BuildType ({
 
     params {
         param("promote.packages.view", "placeholder")
-        param("nuGet.packageFile.database", "EdFi.Suite3.ODS.AdminApp.Database.%adminApp.version%.%build.counter%.nupkg")
-        param("nuGet.packageFile.web", "EdFi.Suite3.ODS.AdminApp.Web.%adminApp.version%.%build.counter%.nupkg")
         param("env.VSS_NUGET_EXTERNAL_FEED_ENDPOINTS", "{\"endpointCredentials\": [{\"endpoint\": \"%azureArtifacts.feed.nuget%\",\"username\": \"%azureArtifacts.edFiBuildAgent.userName%\",\"password\": \"%azureArtifacts.edFiBuildAgent.accessToken%\"}]}")
     }
 
@@ -30,36 +28,6 @@ object PublishReleasePackagesToAzureArtifacts : BuildType ({
     }
 
     steps {
-        powerShell {
-            name = "Publish Application package to Azure Artifacts"
-            formatStderrAsError = true
-            executionMode = BuildStep.ExecutionMode.RUN_ON_SUCCESS
-            scriptMode = script {
-                content = """
-                    ${'$'}arguments = @{
-                    	PackageFile = "%nuGet.packageFile.web%"
-                        EdFiNuGetFeed = "%azureArtifacts.feed.nuget%"
-                        NuGetApiKey = "%azureArtifacts.edFiBuildAgent.accessToken%"
-                    }
-                    ./build.ps1 Push @arguments
-                """.trimIndent()
-            }
-        }
-        powerShell {
-            name = "Publish Database package to Azure Artifacts"
-            formatStderrAsError = true
-            executionMode = BuildStep.ExecutionMode.RUN_ON_SUCCESS
-            scriptMode = script {
-                content = """
-                    ${'$'}arguments = @{
-                    	PackageFile = "%nuGet.packageFile.database%"
-                        EdFiNuGetFeed = "%azureArtifacts.feed.nuget%"
-                        NuGetApiKey = "%azureArtifacts.edFiBuildAgent.accessToken%"
-                    }
-                    ./build.ps1 Push @arguments
-                """.trimIndent()
-            }
-        }
         powerShell {
             name = "Promote Packages"
             formatStderrAsError = true
@@ -86,24 +54,4 @@ object PublishReleasePackagesToAzureArtifacts : BuildType ({
 
 
     // This deliberately does not have a trigger - should only be run manually.
-
-    features {
-        swabra {
-            forceCleanCheckout = true
-        }
-    }
-
-    dependencies {
-        dependency(BuildBranch) {
-            snapshot {
-            }
-
-            artifacts {
-                artifactRules = """
-                    +:%nuGet.packageFile.database% =>
-                    +:%nuGet.packageFile.web% =>
-                """.trimIndent()
-            }
-        }
-    }
 })

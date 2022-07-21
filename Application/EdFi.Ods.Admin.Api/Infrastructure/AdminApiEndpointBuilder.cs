1229a0620f51c0b62d80b83d8f2118ccaa43a70f
Patrick McVeety-Mill <patrick.mcveety-mill@accenture.com>
2022-07-21 09:41:47 -0500

    introduce custom endpoint builder which can account for versions

---
+++ Application/EdFi.Ods.Admin.Api/Infrastructure/AdminApiEndpointBuilder.cs
@@ -0,0 +1,55 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+namespace EdFi.Ods.Admin.Api.Infrastructure;
+
+public class AdminApiEndpointBuilder
+{
+    private AdminApiEndpointBuilder(IEndpointRouteBuilder endpoints,
+        HttpVerb verb, string route, Delegate handler)
+    {
+        _endpoints = endpoints;
+        _verb = verb;
+        _route = route;
+        _handler = handler;
+    }
+
+    private readonly IEndpointRouteBuilder _endpoints;
+    private readonly HttpVerb _verb;
+    private string _route;
+    private Delegate _handler;
+
+    public static AdminApiEndpointBuilder MapGet(IEndpointRouteBuilder endpoints, string route, Delegate handler)
+        => new(endpoints, HttpVerb.GET, route, handler);
+
+    public static AdminApiEndpointBuilder MapPost(IEndpointRouteBuilder endpoints, string route, Delegate handler)
+        => new(endpoints, HttpVerb.POST, route, handler);
+
+    public static AdminApiEndpointBuilder MapPut(IEndpointRouteBuilder endpoints, string route, Delegate handler)
+        => new(endpoints, HttpVerb.PUT, route, handler);
+
+    public static AdminApiEndpointBuilder MapDelete(IEndpointRouteBuilder endpoints, string route, Delegate handler)
+        => new(endpoints, HttpVerb.DELETE, route, handler);
+
+    public void BuildForVersions(params string[] versions)
+    {
+        foreach (var version in versions)
+        {
+            var versionedRoute = $"{version}/{_route}";
+
+            var builder = _verb switch
+            {
+                HttpVerb.GET => _endpoints.MapGet(versionedRoute, _handler),
+                HttpVerb.POST => _endpoints.MapPost(versionedRoute, _handler),
+                HttpVerb.PUT => _endpoints.MapPut(versionedRoute, _handler),
+                HttpVerb.DELETE => _endpoints.MapDelete(versionedRoute, _handler),
+                _ => throw new ArgumentOutOfRangeException($"Unconfigured HTTP verb for mapping: {_}")
+            };
+        }
+    }
+
+    private enum HttpVerb { GET, POST, PUT, DELETE }
+
+}

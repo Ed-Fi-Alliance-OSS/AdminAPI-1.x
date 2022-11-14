b3733c9073ec0417c26e5108b900e3f24b1e82e3
saa14 <saagar.minocha@accenture.com>
2022-11-14 18:11:29 +0530

    Added `uninstall.ps1` for Admin Api uninstallation flow.

---
+++ Installer.AdminApi/uninstall.ps1
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+import-module -force "$PSScriptRoot/Install-AdminApi.psm1"
+
+<#
+This script will uninstall your existing Admin Api installation.
+
+.EXAMPLE
+    $p = @{
+        ToolsPath = "C:/temp/tools"
+        WebSiteName="Ed-Fi-3"
+        WebApplicationPath="c:/inetpub/Ed-Fi-3/AdminApi-3"
+        WebApplicationName = "AdminApi-3"
+    }
+#>
+
+$p = @{
+    ToolsPath = "C:/temp/tools"
+}
+
+Uninstall-AdminApi @p
+

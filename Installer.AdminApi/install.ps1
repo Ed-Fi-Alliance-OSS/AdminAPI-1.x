9693567f28f416de353e622933f4c76dcafa3742
saa14 <saagar.minocha@accenture.com>
2022-11-14 18:06:25 +0530

    Added `install.ps1` for Admin Api installation and `global.json` based on Admin App installer.

---
+++ Installer.AdminApi/install.ps1
@@ -0,0 +1,89 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+import-module -force "$PSScriptRoot/Install-AdminApi.psm1"
+
+<#
+Review and edit the following connection information for your database server
+
+.EXAMPLE
+Installs and connects the applications to the database using SQL Authentication
+
+    $dbConnectionInfo = @{
+        Server = "(local)"
+        Engine = "SqlServer"
+        UseIntegratedSecurity = $false
+        Username = "exampleAdmin"
+        Password = "examplePassword"
+    }
+
+Installs and connects the applications to the database using PostgreSQL Authentication
+
+    $dbConnectionInfo = @{
+        Server = "localhost"
+        Engine = "PostgreSQL"
+        UseIntegratedSecurity = $false
+        Username = "postgres"
+        Password = "examplePassword"
+    }
+#>
+
+$dbConnectionInfo = @{
+    Server = "(local)"
+    Engine = "SqlServer"
+    UseIntegratedSecurity=$true
+}
+
+<#
+Review and edit the following application settings and connection information for Admin App
+
+.EXAMPLE
+Configure Admin Api to manage an ODS API with url "https://localhost:54746"
+
+    $p = @{
+        ToolsPath = "C:/temp/tools"
+        DbConnectionInfo = $dbConnectionInfo
+        OdsApiUrl = "https://localhost:54746"
+        PackageVersion = '2.3.2'
+    }
+
+.EXAMPLE
+Deploy Admin Api for use with a "District Specific" ODS API
+
+    $adminApiFeatures = @{
+        ApiMode = "districtspecifc"
+    }
+
+    $p = @{
+        ToolsPath = "C:/temp/tools"
+        DbConnectionInfo = $dbConnectionInfo
+        OdsApiUrl = "http://web-api.example.com/WebApi"
+        PackageVersion = '2.3.2'
+        AdminApiFeatures = $adminApiFeatures
+    }
+#>
+
+$adminApiFeatures = @{
+    ApiMode = "sharedinstance"
+}
+
+$packageSource = Split-Path $PSScriptRoot -Parent
+$adminApiSource = "$packageSource/AdminApi"
+
+$p = @{
+    ToolsPath = "C:/temp/tools"
+    DbConnectionInfo = $dbConnectionInfo
+    OdsApiUrl = ""
+    PackageVersion = '3.0.1.0'
+    PackageSource = $adminApiSource
+    AdminApiFeatures = $adminApiFeatures
+}
+
+if ([string]::IsNullOrWhiteSpace($p.OdsApiUrl)) {
+    Write-Error "ODS API URL has not been configured. Edit install.ps1 to pass in a valid url for the ODS API."
+}
+else {
+    Install-AdminApi @p
+}

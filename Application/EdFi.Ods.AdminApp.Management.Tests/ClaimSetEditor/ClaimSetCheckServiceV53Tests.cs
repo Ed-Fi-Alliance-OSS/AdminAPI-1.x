14f0f4ccd3be14954a7c8d7dfd7c613cdf13ea71
Patrick McVeety-Mill <patrick.mcveety-mill@accenture.com>
2022-12-15 10:21:14 -0600

    add integration tests to prove service functions for both entities

---
+++ Application/EdFi.Ods.AdminApp.Management.Tests/ClaimSetEditor/ClaimSetCheckServiceV53Tests.cs
@@ -0,0 +1,50 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+extern alias SecurityDataAccess53;
+extern alias SecurityDataAccessLatest;
+
+using System;
+using EdFi.Ods.AdminApp.Management.ClaimSetEditor;
+using NUnit.Framework;
+using Shouldly;
+using static EdFi.Ods.AdminApp.Management.Tests.Testing;
+
+//Test using v53 ClaimSet entity against service using v6 SecurityContext
+using Application = SecurityDataAccess53::EdFi.Security.DataAccess.Models.Application;
+using ClaimSet = SecurityDataAccess53::EdFi.Security.DataAccess.Models.ClaimSet;
+using ConstructContext = SecurityDataAccessLatest::EdFi.Security.DataAccess.Contexts.ISecurityContext;
+
+namespace EdFi.Ods.AdminApp.Management.Tests.ClaimSetEditor;
+
+[TestFixture]
+public class ClaimSetCheckServiceV53Tests : SecurityData53TestBase
+{
+    [Test]
+    public void ShouldReturnTrueWhenClaimSetExists()
+    {
+        var application = new Application { ApplicationName = $"Test Application {DateTime.Now:O}" };
+        Save(application);
+
+        var claimSet = new ClaimSet { ClaimSetName = $"Test ClaimSet {DateTime.Now:O}", Application = application };
+        Save(claimSet);
+
+        Scoped<ConstructContext>(securityContext =>
+        {
+            var service = new ClaimSetCheckService(securityContext);
+            service.ClaimSetExists(claimSet.ClaimSetName).ShouldBeTrue();
+        });
+    }
+
+    [Test]
+    public void ShouldReturnFalseWhenClaimSetDoesNotExist()
+    {
+        Scoped<ConstructContext>(securityContext =>
+        {
+            var service = new ClaimSetCheckService(securityContext);
+            service.ClaimSetExists(Guid.NewGuid().ToString()).ShouldBeFalse();
+        });
+    }
+}
---
+++ Application/EdFi.Ods.AdminApp.Management.Tests/ClaimSetEditor/ClaimSetCheckServiceV6Tests.cs
@@ -0,0 +1,49 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+extern alias SecurityDataAccessLatest;
+
+using EdFi.Ods.AdminApp.Management.ClaimSetEditor;
+using NUnit.Framework;
+using System;
+using SecurityDataAccessLatest::EdFi.Security.DataAccess.Contexts;
+using Shouldly;
+
+using static EdFi.Ods.AdminApp.Management.Tests.Testing;
+
+using Application = SecurityDataAccessLatest::EdFi.Security.DataAccess.Models.Application;
+using ClaimSet = SecurityDataAccessLatest::EdFi.Security.DataAccess.Models.ClaimSet;
+
+namespace EdFi.Ods.AdminApp.Management.Tests.ClaimSetEditor;
+
+[TestFixture]
+public class ClaimSetCheckServiceV6Tests : SecurityDataTestBase
+{
+    [Test]
+    public void ShouldReturnTrueWhenClaimSetExists()
+    {
+        var application = new Application { ApplicationName = $"Test Application {DateTime.Now:O}" };
+        Save(application);
+
+        var claimSet = new ClaimSet { ClaimSetName = $"Test ClaimSet {DateTime.Now:O}", Application = application };
+        Save(claimSet);
+
+        Scoped<ISecurityContext>(securityContext =>
+        {
+            var service = new ClaimSetCheckService(securityContext);
+            service.ClaimSetExists(claimSet.ClaimSetName).ShouldBeTrue();
+        });
+    }
+
+    [Test]
+    public void ShouldReturnFalseWhenClaimSetDoesNotExist()
+    {
+        Scoped<ISecurityContext>(securityContext =>
+        {
+            var service = new ClaimSetCheckService(securityContext);
+            service.ClaimSetExists(Guid.NewGuid().ToString()).ShouldBeFalse();
+        });
+    }
+}

c9f61e4b97a93fb4c18eeb15301bc838b9b62c2e
Stephen A. Fuqua <stephen@safnet.com>
2023-05-27 09:19:32 -0500

    Remove Admin App schema files

--- Application/EdFi.Ods.Admin.Api/Artifacts/MsSql/Structure/Admin/202006121201-AddOdsInstanceIdColumnAndForeignKeyConstraint.sql
+++
@@ -1,7 +0,0 @@
-﻿-- SPDX-License-Identifier: Apache-2.0
--- Licensed to the Ed-Fi Alliance under one or more agreements.
--- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
--- See the LICENSE and NOTICES files in the project root for more information.
-
-ALTER TABLE adminapp.SecretConfigurations ADD OdsInstanceRegistrationId INT NULL;
-ALTER TABLE adminapp.SecretConfigurations ADD CONSTRAINT FK_SecretConfigurations_OdsInstanceRegistrations_Id FOREIGN KEY (OdsInstanceRegistrationId) REFERENCES adminapp.OdsInstanceRegistrations (Id);

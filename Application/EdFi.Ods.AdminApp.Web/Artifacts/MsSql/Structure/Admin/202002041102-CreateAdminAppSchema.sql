f71643e5e3eea8e3d5ba18559e5833950674857d
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2023-05-29 20:47:19 -0500

    [ADMINAPI-98] Build script updates to remove AdminApp (#55)
    * Move artifacts folder
    
    * Remove Admin App schema files
    
    * Rename function and adjust directory path
    
    * Correction to the naming convention
    
    * Remove Admin App functions from build.ps1
    
    * Remove Admin App installer
    
    * Change parameter for consistency with rest of application scripts

--- Application/EdFi.Ods.AdminApp.Web/Artifacts/MsSql/Structure/Admin/202002041102-CreateAdminAppSchema.sql
+++
@@ -1,9 +0,0 @@
-﻿-- SPDX-License-Identifier: Apache-2.0
--- Licensed to the Ed-Fi Alliance under one or more agreements.
--- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
--- See the LICENSE and NOTICES files in the project root for more information.
-
-IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'adminapp')
-BEGIN
-    EXEC( 'CREATE SCHEMA adminapp' );
-END

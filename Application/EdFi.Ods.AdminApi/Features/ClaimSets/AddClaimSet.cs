7fe555b5e037db61dfc80fbdd256bf963ea10e23
Juan Agudelo <jagudelo@wearegap.com>
2023-07-05 14:49:58 -0500

    Adds the POST, PUT, GET all and GET by Id for ClaimSet V2

--- Application/EdFi.Ods.AdminApi/Features/ClaimSets/AddClaimSet.cs
+++ Application/EdFi.Ods.AdminApi/Features/ClaimSets/AddClaimSet.cs
@@ -4,11 +4,9 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using AutoMapper;
+using EdFi.Ods.AdminApi.Features.Applications;
 using EdFi.Ods.AdminApi.Infrastructure;
 using EdFi.Ods.AdminApi.Infrastructure.ClaimSetEditor;
-using EdFi.Ods.AdminApi.Infrastructure.Database.Queries;
-using FluentValidation;
-using Swashbuckle.AspNetCore.Annotations;
 
 namespace EdFi.Ods.AdminApi.Features.ClaimSets;
 
@@ -19,17 +17,17 @@ public class AddClaimSet : IFeature
         AdminApiEndpointBuilder.MapPost(endpoints, "/claimsets", Handle)
         .WithDefaultDescription()
         .WithRouteOptions(b => b.WithResponse<ClaimSetDetailsModel>(201))
-        .BuildForVersions(AdminApiVersions.V1);
+        .BuildForVersions(AdminApiVersions.V2);
     }
 
-    public async Task<IResult> Handle(Validator validator, AddClaimSetCommand addClaimSetCommand,
+    public async Task<IResult> Handle(AddClaimSetValidator validator, AddClaimSetCommand addClaimSetCommand,
         AddOrEditResourcesOnClaimSetCommand addOrEditResourcesOnClaimSetCommand,
         IGetClaimSetByIdQuery getClaimSetByIdQuery,
         IGetResourcesByClaimSetIdQuery getResourcesByClaimSetIdQuery,
         IGetApplicationsByClaimSetIdQuery getApplications,
         IAuthStrategyResolver strategyResolver,
         IMapper mapper,
-        Request request)
+        AddClaimSetRequest request)
     {
         await validator.GuardAsync(request);
         var addedClaimSetId = addClaimSetCommand.Execute(new AddClaimSetModel
@@ -45,65 +43,14 @@ public class AddClaimSet : IFeature
         var claimSet = getClaimSetByIdQuery.Execute(addedClaimSetId);
 
         var model = mapper.Map<ClaimSetDetailsModel>(claimSet);
-        model.ApplicationsCount = getApplications.ExecuteCount(addedClaimSetId);
+        var applications = getApplications.Execute(addedClaimSetId);
+        if (applications != null)
+        {
+            model.Applications = mapper.Map<List<SimpleApplicationModel>>(applications);
+        }
         model.ResourceClaims = getResourcesByClaimSetIdQuery.AllResources(addedClaimSetId)
             .Select(r => mapper.Map<ResourceClaimModel>(r)).ToList();
 
         return AdminApiResponse<ClaimSetDetailsModel>.Created(model, "ClaimSet", $"/claimsets/{addedClaimSetId}");
-    }
-
-    [SwaggerSchema(Title = "AddClaimSetRequest")]
-    public class Request
-    {
-        [SwaggerSchema(Description = FeatureConstants.ClaimSetNameDescription, Nullable = false)]
-        public string? Name { get; set; }
-
-        [SwaggerSchema(Description = FeatureConstants.ResourceClaimsDescription, Nullable = false)]
-        public List<ResourceClaimModel>? ResourceClaims { get; set; }
-    }
-
-    public class Validator : AbstractValidator<Request>
-    {
-        private readonly IGetAllClaimSetsQuery _getAllClaimSetsQuery;
-
-        public Validator(IGetAllClaimSetsQuery getAllClaimSetsQuery,
-            IGetResourceClaimsAsFlatListQuery getResourceClaimsAsFlatListQuery,
-            IGetAllAuthorizationStrategiesQuery getAllAuthorizationStrategiesQuery)
-        {
-            _getAllClaimSetsQuery = getAllClaimSetsQuery;
-
-            var resourceClaims = (Lookup<string, ResourceClaim>)getResourceClaimsAsFlatListQuery.Execute()
-                .ToLookup(rc => rc.Name?.ToLower());
-
-            var authStrategyNames = getAllAuthorizationStrategiesQuery.Execute()
-                .Select(a => a.AuthStrategyName).ToList();
-
-            RuleFor(m => m.Name).NotEmpty()
-                .Must(BeAUniqueName)
-                .WithMessage(FeatureConstants.ClaimSetAlreadyExistsMessage);
-
-            RuleFor(m => m.Name)
-                .MaximumLength(255)
-                .WithMessage(FeatureConstants.ClaimSetNameMaxLengthMessage);
-
-            RuleFor(m => m).Custom((claimSet, context) =>
-            {
-                var resourceClaimValidator = new ResourceClaimValidator();
-
-                if (claimSet.ResourceClaims != null && claimSet.ResourceClaims.Any())
-                {
-                    foreach (var resourceClaim in claimSet.ResourceClaims)
-                    {
-                        resourceClaimValidator.Validate(resourceClaims, authStrategyNames,
-                            resourceClaim, claimSet.ResourceClaims, context, claimSet.Name);
-                    }
-                }
-            });
-        }
-
-        private bool BeAUniqueName(string? name)
-        {
-            return _getAllClaimSetsQuery.Execute().All(x => x.Name != name);
-        }
     }
 }

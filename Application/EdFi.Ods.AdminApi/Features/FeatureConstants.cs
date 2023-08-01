94ad493b2191a96cdd4e8819ea06f0c26b42c6e1
CSR2017 <suganya@imanisoftware.com>
2023-08-01 17:00:35 -0500

    Add integration test

--- Application/EdFi.Ods.AdminApi/Features/FeatureConstants.cs
+++ Application/EdFi.Ods.AdminApi/Features/FeatureConstants.cs
@@ -30,5 +30,5 @@ public class FeatureConstants
     public const string ClaimSetNotFound = "No such claim set exists in the database.";
     public const string OdsInstanceIdDescription = "ODS instance id";
     public const string OdsInstanceIdValidationMessage = "Please provide valid ods instance id.";
-    public const string ClaimSetIdToCopy = "ClaimSet id for copying";
+    public const string ClaimSetIdToCopy = "ClaimSet id to copy";
 }

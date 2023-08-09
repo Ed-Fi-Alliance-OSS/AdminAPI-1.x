9ab35a320c49a12718f67548e0ed2f21c6b85ba8
Luis Chavarriaga <lchavarriaga@growthaccelerationpartners.com>
2023-08-09 16:41:54 -0500

    AdminAPI-450 | ConnectionString updated

--- Docker/Settings/dev/adminapi-test-seeddata.sql
+++ Docker/Settings/dev/adminapi-test-seeddata.sql
@@ -1,3 +1,3 @@
 -- LOAD DATAS
 INSERT INTO dbo.odsinstances(name, instancetype, connectionstring)
-VALUES ('Ods-test', 'OdsInstance', 'server=pb-ods;database=EdFi_Ods;application name=EdFi.Ods.AdminApp');
+VALUES ('Ods-test', 'OdsInstance', 'Host=localhost; Port=5432; Database=EdFi_Ods; username=username; Integrated Security=true; Application Name=AdminApi;');

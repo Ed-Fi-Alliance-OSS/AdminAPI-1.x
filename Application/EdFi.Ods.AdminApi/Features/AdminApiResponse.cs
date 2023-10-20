ce2816ae41132228b3d80bf88ab7ad00f81e5bfc
Juan Agudelo <jagudelo@wearegap.com>
2023-10-20 12:07:08 -0500

    Set as nullable jsonSerializerSettings

--- Application/EdFi.Ods.AdminApi/Features/AdminApiResponse.cs
+++ Application/EdFi.Ods.AdminApi/Features/AdminApiResponse.cs
@@ -36,34 +36,50 @@ public class AdminApiResponse<T> : AdminApiResponse
     protected AdminApiResponse(int status, T result) : base(status) { Result = result; }
     protected AdminApiResponse(int status, string title, T result) : base(status, title) { Result = result; }
 
-    public static IResult Ok(T result)
-        => Results.Ok(new AdminApiResponse<T>(200, "Request successful", result));
-
-    public static IResult Ok(T result, JsonSerializerSettings jsonSerializerSettings)
+    public static IResult Ok(T result, JsonSerializerSettings? jsonSerializerSettings = null)
     {
         var statusCode = HttpStatusCode.OK;
-        var jsonResponse = SerializeObjectAsJson(result, statusCode, "Request successful", jsonSerializerSettings);
-        return new JsonSerializeResult(jsonResponse, statusCode);
+        var message = "Request successful";
+        if (jsonSerializerSettings != null)
+        {
+            var jsonResponse = SerializeObjectAsJson(result, statusCode, message, jsonSerializerSettings);
+            return new JsonSerializeResult(jsonResponse, statusCode);
+        }
+        else
+        {
+            return Results.Ok(new AdminApiResponse<T>((int)statusCode, message, result));
+        }
     }
 
-    public static IResult Created(T result, string name, string getUri)
-        => Results.Created(getUri, new AdminApiResponse<T>(201, $"{name} created successfully", result));
-
-    public static IResult Created(T result, string name, string getUri, JsonSerializerSettings jsonSerializerSettings)
+    public static IResult Created(T result, string name, string getUri, JsonSerializerSettings? jsonSerializerSettings = null)
     {
         var statusCode = HttpStatusCode.Created;
-        var jsonResponse = SerializeObjectAsJson(result, statusCode, $"{name} created successfully", jsonSerializerSettings);
-        return new JsonSerializeResult(jsonResponse, statusCode, getUri);
+        var message = $"{name} created successfully";
+        if (jsonSerializerSettings != null)
+        {
+            var jsonResponse = SerializeObjectAsJson(result, statusCode, message, jsonSerializerSettings);
+            return new JsonSerializeResult(jsonResponse, statusCode, getUri);
+        }
+        else
+        {
+            return Results.Created(getUri, new AdminApiResponse<T>((int)statusCode, message, result)); ;
+        }
     }
 
-    public static IResult Updated(T result, string name)
-        => Results.Ok(new AdminApiResponse<T>(200, $"{name} updated successfully", result));
-
-    public static IResult Updated(T result, string name, JsonSerializerSettings jsonSerializerSettings)
+    public static IResult Updated(T result, string name, JsonSerializerSettings? jsonSerializerSettings = null)
     {
         var statusCode = HttpStatusCode.OK;
-        var jsonResponse = SerializeObjectAsJson(result, statusCode, $"{name} updated successfully", jsonSerializerSettings);
-        return new JsonSerializeResult(jsonResponse, statusCode);
+        var message = $"{name} updated successfully";
+
+        if (jsonSerializerSettings != null)
+        {
+            var jsonResponse = SerializeObjectAsJson(result, statusCode, message, jsonSerializerSettings);
+            return new JsonSerializeResult(jsonResponse, statusCode);
+        }
+        else
+        {
+            return Results.Ok(new AdminApiResponse<T>((int)statusCode, message, result));
+        }
     }
 
     private static string SerializeObjectAsJson(T result, HttpStatusCode statusCode, string message, JsonSerializerSettings jsonSerializerSettings)

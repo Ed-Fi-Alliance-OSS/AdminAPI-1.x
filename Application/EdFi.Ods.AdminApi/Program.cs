f0e7791e68f036f5d4f2dae4c241324d1b5363a2
Luis Chavarriaga <lchavarriaga@growthaccelerationpartners.com>
2023-12-14 17:45:43 -0500

    ADMINAPI-764 | Rate Limiting

--- Application/EdFi.Ods.AdminApi/Program.cs
+++ Application/EdFi.Ods.AdminApi/Program.cs
@@ -3,12 +3,22 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using AspNetCoreRateLimit;
 using EdFi.Ods.AdminApi.Features;
 using EdFi.Ods.AdminApi.Infrastructure;
 using EdFi.Ods.AdminApi.Infrastructure.MultiTenancy;
 using log4net;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Rate Limit
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddInMemoryRateLimiting();
+
 
 builder.AddServices();
 
@@ -17,6 +27,7 @@ var _logger = LogManager.GetLogger("Program");
 _logger.Info("Starting Admin API");
 
 var app = builder.Build();
+
 
 var pathBase = app.Configuration.GetValue<string>("AppSettings:PathBase");
 if (!string.IsNullOrEmpty(pathBase))
@@ -27,6 +38,7 @@ if (!string.IsNullOrEmpty(pathBase))
 
 AdminApiVersions.Initialize(app);
 
+app.UseIpRateLimiting();
 //The ordering here is meaningful: Logging -> Routing -> Auth -> Endpoints
 app.UseMiddleware<RequestLoggingMiddleware>();
 app.UseMiddleware<TenantResolverMiddleware>();

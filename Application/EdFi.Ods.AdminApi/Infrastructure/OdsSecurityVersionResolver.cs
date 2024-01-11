5182ff012ac0eb0d1ef0d0e97075d42f5b5d6c35
Adam Hopkins <adam.hopkins@simpat.tech>
2024-01-11 12:19:49 -0600

    retrieve ods api version from appsettings

--- Application/EdFi.Ods.AdminApi/Infrastructure/OdsSecurityVersionResolver.cs
+++ Application/EdFi.Ods.AdminApi/Infrastructure/OdsSecurityVersionResolver.cs
@@ -3,7 +3,6 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using EdFi.Ods.AdminApi.Infrastructure.Api;
 using log4net;
 
 namespace EdFi.Ods.AdminApi.Infrastructure;
@@ -17,21 +16,11 @@ public class OdsSecurityVersionResolver : IOdsSecurityModelVersionResolver
 {
     private readonly ILog _log;
     private readonly Lazy<EdFiOdsSecurityModelCompatibility> _modelVersion;
-    private readonly IOdsApiValidator _apiValidator;
-    private readonly string _apiServerUrl;
+    private readonly string _odsApiVersion;
 
-    // In ODS/API version 5.3 there is a special branch/tag that provides
-    // additional security functionality, which was originally developed for
-    // ODS/API 6.1. In general, the Admin and Security databases still look like
-    // ODS/API 5.3, but we need to know that it is in fact "CQE" so that we can
-    // handle the "ReadChanges" action properly.
-    private readonly bool _using53Cqe;
-
-    public OdsSecurityVersionResolver(IOdsApiValidator apiValidator, string apiServerUrl, bool using53Cqe = false)
+    public OdsSecurityVersionResolver(string odsApiVersion)
     {
-        _apiValidator = apiValidator;
-        _apiServerUrl = apiServerUrl;
-        _using53Cqe = using53Cqe;
+        _odsApiVersion = odsApiVersion;
         _log = LogManager.GetLogger(typeof(OdsSecurityVersionResolver));
         _modelVersion = new Lazy<EdFiOdsSecurityModelCompatibility>(InitializeModelVersion);
     }
@@ -40,24 +29,20 @@ public class OdsSecurityVersionResolver : IOdsSecurityModelVersionResolver
 
     private EdFiOdsSecurityModelCompatibility InitializeModelVersion()
     {
-        var validationResult = _apiValidator.Validate(_apiServerUrl).GetAwaiter().GetResult();
-
-        if (!validationResult.IsValidOdsApi || validationResult.Version == null)
+        switch (_odsApiVersion)
         {
-            _log.Error("Unable to determine security model from ODS API Response");
-            throw validationResult.Exception ?? new Exception("No version reported from the Ed-Fi API");
+            case "5.3":
+                return EdFiOdsSecurityModelCompatibility.ThreeThroughFive;
+            case "5.3cqe":
+                return EdFiOdsSecurityModelCompatibility.FiveThreeCqe;
+            case "6.0":
+            case "6.1":
+                return EdFiOdsSecurityModelCompatibility.Six;
+            default:
+                _log.Error("OdsApiVersion not configured. Valid values are 5.3, 5.3cqe, 6.0 and 6.1");
+                throw new Exception("OdsApiVersion not configured. Valid values are 5.3, 5.3cqe, 6.0 and 6.1");
+                
         }
-
-        if (_using53Cqe)
-        {
-            return EdFiOdsSecurityModelCompatibility.FiveThreeCqe;
-        }
-
-        var serverVersion = validationResult.Version;
-
-        return serverVersion.Major < 6
-            ? EdFiOdsSecurityModelCompatibility.ThreeThroughFive
-            : EdFiOdsSecurityModelCompatibility.Six;
     }
 }
 

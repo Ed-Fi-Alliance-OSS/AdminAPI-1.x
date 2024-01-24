9d11ba3a97010c671f6aca0fd9badb644f630696
Juan Agudelo <jagudelo@wearegap.com>
2024-01-24 16:22:40 -0500

    Revert dummy changes

--- Application/EdFi.Ods.AdminApi/Features/ClaimSets/ResourceClaimValidator.cs
+++ Application/EdFi.Ods.AdminApi/Features/ClaimSets/ResourceClaimValidator.cs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
-// See the LICENSE and NOTICES files in the project root for more information. 
+// See the LICENSE and NOTICES files in the project root for more information.
 
 using EdFi.Ods.AdminApi.Infrastructure.ClaimSetEditor;
 using FluentValidation;

5619df089bb4a3ce6c9371d576fde9bcebb37597
David Jimenez <djimenez@growthaccelerationpartners.com>
2024-09-12 11:48:58 -0600

    Working on e2e tests, Vendors and Applications. With other fixes.

--- Application/EdFi.Ods.AdminApi/Infrastructure/Database/Queries/GetApplicationsQuery.cs
+++ Application/EdFi.Ods.AdminApi/Infrastructure/Database/Queries/GetApplicationsQuery.cs
@@ -8,48 +8,46 @@ using EdFi.Admin.DataAccess.Models;
 using EdFi.Ods.AdminApi.Infrastructure.Extensions;
 using Microsoft.EntityFrameworkCore;
 
-namespace EdFi.Ods.AdminApi.Infrastructure.Database.Queries
+namespace EdFi.Ods.AdminApi.Infrastructure.Database.Queries;
+public interface IGetApplicationsQuery
 {
-    public interface IGetApplicationsQuery
+    List<Application> Execute();
+    List<Application> Execute(CommonQueryParams commonQueryParams);
+}
+
+public class GetApplicationsQuery : IGetApplicationsQuery
+{
+    private readonly IUsersContext _context;
+
+    public GetApplicationsQuery(IUsersContext context)
     {
-        List<Application> Execute();
-        List<Application> Execute(CommonQueryParams commonQueryParams);
+        _context = context;
     }
 
-    public class GetApplicationsQuery : IGetApplicationsQuery
+    public List<Application> Execute()
     {
-        private readonly IUsersContext _context;
+        return _context.Applications
+            .Include(ap => ap.Vendor).ThenInclude(ap => ap.VendorNamespacePrefixes)
+            .Include(ap => ap.Vendor).ThenInclude(ap => ap.Users)
+            .Include(ap => ap.Profiles)
+            .Include(ap => ap.OdsInstance)
+            .Include(ap => ap.ApplicationEducationOrganizations)
+            .OrderBy(v => v.Vendor.VendorName)
+            .Where(v => !VendorExtensions.ReservedNames.Contains(v.Vendor.VendorName.Trim()))
+            .ToList();
+    }
 
-        public GetApplicationsQuery(IUsersContext context)
-        {
-            _context = context;
-        }
-
-        public List<Application> Execute()
-        {
-            return _context.Applications
-                .Include(ap => ap.Vendor).ThenInclude(ap => ap.VendorNamespacePrefixes)
-                .Include(ap => ap.Vendor).ThenInclude(ap => ap.Users)
-                .Include(ap => ap.Profiles)
-                .Include(ap => ap.OdsInstance)
-                .Include(ap => ap.ApplicationEducationOrganizations)
-                .OrderBy(v => v.Vendor.VendorName)
-                .Where(v => !VendorExtensions.ReservedNames.Contains(v.Vendor.VendorName.Trim()))
-                .ToList();
-        }
-
-        public List<Application> Execute(CommonQueryParams commonQueryParams)
-        {
-            return _context.Applications
-                .Include(ap => ap.Vendor).ThenInclude(ap => ap.VendorNamespacePrefixes)
-                .Include(ap => ap.Vendor).ThenInclude(ap => ap.Users)
-                .Include(ap => ap.Profiles)
-                .Include(ap => ap.OdsInstance)
-                .Include(ap => ap.ApplicationEducationOrganizations)
-                .OrderBy(v => v.Vendor.VendorName)
-                .Where(v => !VendorExtensions.ReservedNames.Contains(v.Vendor.VendorName.Trim()))
-                .Paginate(commonQueryParams.Offset, commonQueryParams.Limit)
-                .ToList();
-        }
+    public List<Application> Execute(CommonQueryParams commonQueryParams)
+    {
+        return _context.Applications
+            .Include(ap => ap.Vendor).ThenInclude(ap => ap.VendorNamespacePrefixes)
+            .Include(ap => ap.Vendor).ThenInclude(ap => ap.Users)
+            .Include(ap => ap.Profiles)
+            .Include(ap => ap.OdsInstance)
+            .Include(ap => ap.ApplicationEducationOrganizations)
+            .OrderBy(v => v.ApplicationName)
+            .Where(v => !VendorExtensions.ReservedNames.Contains(v.Vendor.VendorName.Trim()))
+            .Paginate(commonQueryParams.Offset, commonQueryParams.Limit)
+            .ToList();
     }
 }
--- Application/EdFi.Ods.AdminApi/Infrastructure/Extensions/QueryExtensions.cs
+++ Application/EdFi.Ods.AdminApi/Infrastructure/Extensions/QueryExtensions.cs
@@ -1,35 +1,31 @@
-using System.Linq.Expressions;
-
-namespace EdFi.Ods.AdminApi.Infrastructure.Extensions
+namespace EdFi.Ods.AdminApi.Infrastructure.Extensions;
+public static class QueryExtensions
 {
-    public static class QueryExtensions
+    /// <summary>
+    /// Apply pagination based on the offset and limit
+    /// </summary>
+    /// <typeparam name="T">Type of entity</typeparam>
+    /// <param name="source">IQueryable entity list to apply the pagination</param>
+    /// <param name="offset"></param>
+    /// <param name="limit"></param>
+    /// <param name="settings">App Setting values</param>
+    /// <returns>Paginated list</returns>
+    public static IQueryable<T> Paginate<T>(this IQueryable<T> source, int? offset, int? limit)
     {
-        /// <summary>
-        /// Apply pagination based on the offset and limit
-        /// </summary>
-        /// <typeparam name="T">Type of entity</typeparam>
-        /// <param name="source">IQueryable entity list to apply the pagination</param>
-        /// <param name="offset"></param>
-        /// <param name="limit"></param>
-        /// <param name="settings">App Setting values</param>
-        /// <returns>Paginated list</returns>
-        public static IQueryable<T> Paginate<T>(this IQueryable<T> source, int? offset, int? limit)
+        try
         {
-            try
-            {
-                if (offset != null)
-                    source = source.Skip(offset.Value);
+            if (offset != null)
+                source = source.Skip(offset.Value);
 
-                if (limit != null)
-                    source = source.Take(limit.Value);
+            if (limit != null)
+                source = source.Take(limit.Value);
 
-                return source;
-            }
-            catch (Exception)
-            {
-                // If this throws an exception simply don't paginate.
-                return source;
-            }
+            return source;
+        }
+        catch (Exception)
+        {
+            // If this throws an exception simply don't paginate.
+            return source;
         }
     }
 }

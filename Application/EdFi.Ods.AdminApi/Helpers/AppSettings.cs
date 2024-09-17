7be0f93ab02ee366d78db5ba50389670ce6648f3
David Jimenez <djimenez@growthaccelerationpartners.com>
2024-09-17 10:15:28 -0600

    Cleanup on AppSettings.cs

--- Application/EdFi.Ods.AdminApi/Helpers/AppSettings.cs
+++ Application/EdFi.Ods.AdminApi/Helpers/AppSettings.cs
@@ -8,13 +8,4 @@ public class AppSettings
 {
     public int DefaultPageSizeOffset { get; set; }
     public int DefaultPageSizeLimit { get; set; }
-    public string? DatabaseEngine { get; set; }
-    public bool MultiTenancy { get; set; }
-    public bool PreventDuplicateApplications { get; set; }
-}
-
-public class SwaggerSettings
-{
-    public bool EnableSwagger { get; set; }
-    public string? DefaultTenant { get; set; }
 }

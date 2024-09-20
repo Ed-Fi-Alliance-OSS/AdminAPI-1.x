99c67e80f58d6399a7f650a4afdc84c9abd5920d
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2024-09-19 18:01:10 -0600

    Update documentation
    Update documents
    Fix actions errors

--- build.ps1
+++ build.ps1
@@ -393,7 +393,7 @@ function RestartAdminApiContainer {
 }
 
 function BuildAdminApiDevDockerImage {
-    &docker build -t adminapi-dev --no-cache -f "$dockerRoot/dev.Dockerfile" .
+    &docker build -t adminapi-dev --no-cache -f "$dockerRoot/dev.pgsql.Dockerfile" .
 }
 
 function RunAdminApiDevDockerContainer {

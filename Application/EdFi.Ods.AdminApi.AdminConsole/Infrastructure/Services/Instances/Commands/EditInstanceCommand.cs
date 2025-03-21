313713aecb5d9b8b2e106982677c3ab41a737142
Danny Fernandez A <129120407+dfernandez-gap@users.noreply.github.com>
2025-03-21 15:53:47 -0600

    [ADMINAPI-1175] - PUT /adminconsole/odsInstances/{id} to Mark Status When Name is Changed (#268)
    * Add Mark Instance for Rename
    
    * Fix tests
    
    * Add Mark Instance for Rename
    
    * Fix tests
    
    * Add Status Validation
    Add Unit test
    
    * Add Mark Instance for Rename
    
    * Fix tests
    
    * Add Status Validation
    Add Unit test

--- Application/EdFi.Ods.AdminApi.AdminConsole/Infrastructure/Services/Instances/Commands/EditInstanceCommand.cs
+++ Application/EdFi.Ods.AdminApi.AdminConsole/Infrastructure/Services/Instances/Commands/EditInstanceCommand.cs
@@ -3,6 +3,9 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System.Transactions;
+using EdFi.Admin.DataAccess.Contexts;
+using EdFi.Admin.DataAccess.Models;
 using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.DataAccess.Contexts;
 using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.DataAccess.Models;
 using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Repositories;
@@ -17,15 +20,45 @@ namespace EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Services.Instances.Comma
         Task<Instance> Execute(int id, IInstanceRequestModel instance);
     }
 
-    public class EditInstanceCommand(ICommandRepository<Instance> instanceCommand, IQueriesRepository<Instance> instanceQuery, IDbContext dbContext) : IEditInstanceCommand
+    public class EditInstanceCommand(ICommandRepository<Instance> instanceCommand, IQueriesRepository<Instance> instanceQuery, IDbContext dbContext, IUsersContext context) : IEditInstanceCommand
     {
         private readonly ICommandRepository<Instance> _instanceCommand = instanceCommand;
         private readonly IQueriesRepository<Instance> _instanceQuery = instanceQuery;
         private readonly IDbContext _dbContext = dbContext;
+        private readonly IUsersContext _context = context;
 
         public async Task<Instance> Execute(int id, IInstanceRequestModel instance)
         {
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
             var existingInstance = await _instanceQuery.Query().SingleOrDefaultAsync(w => w.Id == id) ?? throw new NotFoundException<int>("Instance", id);
+
+            if (!instance.Name.Equals(existingInstance.InstanceName, StringComparison.InvariantCultureIgnoreCase) && instance.Status == InstanceStatus.Completed.ToString())
+            {
+                existingInstance.Status = InstanceStatus.Pending_Rename;
+
+                var odsInstance = await _context.OdsInstances
+                    .Include(i => i.OdsInstanceContexts)
+                    .Include(i => i.OdsInstanceDerivatives)
+                    .SingleOrDefaultAsync(w => w.OdsInstanceId == existingInstance.OdsInstanceId);
+
+                var apiclientOdsInstance = await _context.ApiClientOdsInstances
+                    .Include(i => i.ApiClient)
+                    .SingleOrDefaultAsync(w => w.OdsInstance.OdsInstanceId == existingInstance.OdsInstanceId);
+
+                if (apiclientOdsInstance != null)
+                {
+                    _context.ApiClientOdsInstances.Remove(apiclientOdsInstance);
+                    _context.ApiClients.Remove(apiclientOdsInstance.ApiClient);
+                }
+
+                if (odsInstance != null)
+                {
+                    _context.OdsInstanceContexts.RemoveRange(odsInstance.OdsInstanceContexts);
+                    _context.OdsInstanceDerivatives.RemoveRange(odsInstance.OdsInstanceDerivatives);
+                    _context.OdsInstances.Remove(odsInstance);
+                }
+                _context.SaveChanges();
+            }
 
             existingInstance.OdsInstanceId = instance.OdsInstanceId;
             existingInstance.TenantId = instance.TenantId;
@@ -35,6 +68,8 @@ namespace EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Services.Instances.Comma
             await UpdateOdsInstanceDerivativesAsync(id, instance.TenantId, instance.OdsInstanceDerivatives);
             await UpdateOdsInstanceContextsAsync(id, instance.TenantId, instance.OdsInstanceContexts);
             await _instanceCommand.UpdateAsync(existingInstance);
+
+            scope.Complete();
             return existingInstance;
         }
 
@@ -53,7 +88,7 @@ namespace EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Services.Instances.Comma
                 _dbContext.OdsInstanceDerivatives.RemoveRange(existingOdsInstanceDerivatives);
                 foreach (var updatedItem in updatedOdsInstanceDerivatives)
                 {
-                    _dbContext.OdsInstanceDerivatives.Add(new OdsInstanceDerivative
+                    _dbContext.OdsInstanceDerivatives.Add(new DataAccess.Models.OdsInstanceDerivative
                     {
                         InstanceId = id,
                         TenantId = tenantId,
@@ -80,7 +115,7 @@ namespace EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Services.Instances.Comma
                 _dbContext.OdsInstanceContexts.RemoveRange(existingOdsInstanceContexts);
                 foreach (var updatedContext in updatedOdsInstanceContexts)
                 {
-                    _dbContext.OdsInstanceContexts.Add(new OdsInstanceContext
+                    _dbContext.OdsInstanceContexts.Add(new DataAccess.Models.OdsInstanceContext
                     {
                         InstanceId = id,
                         TenantId = tenantId,
--- Application/EdFi.Ods.AdminConsole.DBTests/Database/CommandTests/EditInstanceCommandTests.cs
+++ Application/EdFi.Ods.AdminConsole.DBTests/Database/CommandTests/EditInstanceCommandTests.cs
@@ -10,13 +10,16 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using EdFi.Admin.DataAccess.Models;
+using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.DataAccess.Contexts.Admin.MsSql;
 using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.DataAccess.Models;
 using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Repositories;
 using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Services;
 using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Services.Instances.Commands;
 using EdFi.Ods.AdminApi.AdminConsole.Infrastructure.Services.Instances.Models;
+using EdFi.Ods.AdminApi.Common.Infrastructure.Database;
 using EdFi.Ods.AdminApi.Infrastructure.Database.Commands;
 using Microsoft.EntityFrameworkCore;
 using Moq;
@@ -40,6 +43,7 @@ public class EditInstanceCommandTests : PlatformUsersContextTestBase
             OdsInstanceId = 1,
             InstanceName = "Test Instance",
             InstanceType = "Standard",
+            Status = InstanceStatus.Completed,
             OdsInstanceContexts = new List<AdminApi.AdminConsole.Infrastructure.DataAccess.Models.OdsInstanceContext>(),
             OdsInstanceDerivatives = new List<AdminApi.AdminConsole.Infrastructure.DataAccess.Models.OdsInstanceDerivative>()
         };
@@ -50,6 +54,7 @@ public class EditInstanceCommandTests : PlatformUsersContextTestBase
     [Test]
     public void ShouldEditInstance()
     {
+        AdminConsoleSqlServerUsersContext userDbContext = new(GetUserDbContextOptions());
         var newInstanceData = new Mock<IInstanceRequestModel>();
         newInstanceData.Setup(v => v.OdsInstanceId).Returns(1);
         newInstanceData.Setup(v => v.TenantId).Returns(1);
@@ -63,7 +68,7 @@ public class EditInstanceCommandTests : PlatformUsersContextTestBase
         {
             var repository = new CommandRepository<Instance>(dbContext);
             var qRepository = new QueriesRepository<Instance>(dbContext);
-            var command = new EditInstanceCommand(repository, qRepository, dbContext);
+            var command = new EditInstanceCommand(repository, qRepository, dbContext, userDbContext);
 
             var result = await command.Execute(_odsInstanceId, newInstanceData.Object);
         });
@@ -78,6 +83,43 @@ public class EditInstanceCommandTests : PlatformUsersContextTestBase
             instance.OdsInstanceId.ShouldBe(1);
             instance.InstanceName.ShouldBe("Updated Instance");
             instance.InstanceType.ShouldBe("Updated Type");
+            instance.OdsInstanceContexts.ShouldBeEmpty();
+            instance.OdsInstanceDerivatives.ShouldBeEmpty();
+        });
+    }
+
+    [Test]
+    public void ShouldSetPendingRenameStatusWhenNameChanges()
+    {
+        AdminConsoleSqlServerUsersContext userDbContext = new(GetUserDbContextOptions());
+        var newInstanceData = new Mock<IInstanceRequestModel>();
+        newInstanceData.Setup(v => v.OdsInstanceId).Returns(1);
+        newInstanceData.Setup(v => v.TenantId).Returns(1);
+        newInstanceData.Setup(v => v.TenantName).Returns("UpdateTenantName");
+        newInstanceData.Setup(v => v.Name).Returns("Updated Instance");
+        newInstanceData.Setup(v => v.InstanceType).Returns("Updated Type");
+        newInstanceData.Setup(v => v.OdsInstanceContexts).Returns(new List<OdsInstanceContextModel>());
+        newInstanceData.Setup(v => v.OdsInstanceDerivatives).Returns(new List<OdsInstanceDerivativeModel>());
+
+        Transaction(async dbContext =>
+        {
+            var repository = new CommandRepository<Instance>(dbContext);
+            var qRepository = new QueriesRepository<Instance>(dbContext);
+            var command = new EditInstanceCommand(repository, qRepository, dbContext, userDbContext);
+            var result = await command.Execute(_odsInstanceId, newInstanceData.Object);
+        });
+
+        Transaction(dbContext =>
+        {
+            var persistedInstance = dbContext.Instances;
+            persistedInstance.Count().ShouldBe(1);
+            var instance = persistedInstance.First();
+            instance.TenantId.ShouldBe(1);
+            instance.TenantName.ShouldBe("UpdateTenantName");
+            instance.OdsInstanceId.ShouldBe(1);
+            instance.InstanceName.ShouldBe("Updated Instance");
+            instance.InstanceType.ShouldBe("Updated Type");
+            instance.Status.ToString().ShouldBe(InstanceStatus.Pending_Rename.ToString());
             instance.OdsInstanceContexts.ShouldBeEmpty();
             instance.OdsInstanceDerivatives.ShouldBeEmpty();
         });

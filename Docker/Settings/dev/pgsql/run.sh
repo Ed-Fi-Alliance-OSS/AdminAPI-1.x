a938a8ea0fb5a1f54685bd8eb305aaf9e29ecfa3
Jorge David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2025-06-03 15:07:24 -0600

    [ADMINAPI-1210] - Fix for pgsql multitenant (#302)
    Fix for pgsql multitenant - Follow up ticket - Refactor Admin Console database install to use the same mechanism as with Admin API tables

--- Docker/Settings/dev/pgsql/run.sh
+++ Docker/Settings/dev/pgsql/run.sh
@@ -11,6 +11,28 @@ envsubst < /app/appsettings.Docker.json > /app/temp.json
 
 mv /app/temp.json /app/appsettings.json
 
+if [[ -z "$ADMIN_WAIT_POSTGRES_HOSTS" ]]; then
+  # if there are no hosts to wait then fallback to $ODS_POSTGRES_HOST
+  export ADMIN_WAIT_POSTGRES_HOSTS=$ADMIN_POSTGRES_HOST
+fi
+
+export ADMIN_WAIT_POSTGRES_HOSTS_ARR=($ADMIN_WAIT_POSTGRES_HOSTS)
+for HOST in ${ADMIN_WAIT_POSTGRES_HOSTS_ARR[@]}
+do
+  until PGPASSWORD=$POSTGRES_PASSWORD \
+      PGHOST=$HOST \
+      PGPORT=$POSTGRES_PORT \
+      PGUSER=$POSTGRES_USER \
+      pg_isready > /dev/null
+  do
+    >&2 echo "Admin '$HOST' is unavailable - sleeping"
+    sleep 10
+  done
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
+
 if [[ -f /ssl/server.crt ]]; then
  cp /ssl/server.crt /usr/local/share/ca-certificates/
  update-ca-certificates
